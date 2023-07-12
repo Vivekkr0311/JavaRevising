06816b36e7abb80bf86a56c778548c8f8fe4e9ec
Vivek Kumar <vivekkr0311@gmail.com>
2023-07-12 20:16:43 +0530

    Java array and arraylist add

---
+++ src/com/Vivek/ArraysAndArrayLists/DynamicArrays.java
@@ -0,0 +1,43 @@
+package com.Vivek.ArraysAndArrayLists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Dynamic arrays is just a fancy word to ArrayList.
+public class DynamicArrays {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = makesAnArraylist();
+        System.out.println(arr);
+    }
+
+    static ArrayList<Integer> makesAnArraylist(){
+        boolean stillWantToFillElements = true;
+        Scanner flag = new Scanner(System.in);
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("[?] How many elements?: ");
+        int n = in.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        //In '()' you can specify size as well, like new ArrayList<>(n);
+        //But as we demonstrating dynamic size array, we will not do that.
+        System.out.print("[?] Inserting individual elements: ");
+        for(int i = 0; i < n; i++){
+            arr.add(in.nextInt());
+        }
+
+
+        while(stillWantToFillElements){
+            System.out.print("[?] Do you still want to fill elements?: (true or false) ");
+            stillWantToFillElements = flag.nextBoolean();
+            if(stillWantToFillElements){
+                System.out.print("[+] Adding some more elements: ");
+                arr.add(in.nextInt());
+            }else{
+                break;
+            }
+        }
+
+        return arr;
+    }
+}
