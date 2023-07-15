a98dd2133f84b073436004cf445331394ab6e42d
Vivek Kumar <vivekkr0311@gmail.com>
2023-07-15 20:54:27 +0530

    Optimized Count Digits In An Array

--- src/com/Vivek/LinearSearch/CountDigitsInAnArray.java
+++ src/com/Vivek/LinearSearch/CountDigitsInAnArray.java
@@ -24,13 +24,28 @@ public class CountDigitsInAnArray {
         return arr;
     }
     static int countDigit(int number){
+        int num = number;
+        if(number < 0){
+            num = number*(-1);
+        }
+
         int count = 0;
-        while(number > 0){
+        while(num > 0){
             count++;
-            number /= 10;
+            num /= 10;
         }
 
         return count;
+    }
+
+    static int countDigit2(int number){
+        int num = number;
+        if(number < 0){
+            num = number*(-1);
+        }
+        //This efficient way to calculate number of digits of a number.
+        //It just basically gives answer (log of <number> with base = 10) + 1.
+        return (int)Math.log10(num) + 1;
     }
 
     static int numberOfDigitHavingEvenNumberOfDigits(int arr[]){
@@ -41,8 +56,8 @@ public class CountDigitsInAnArray {
 
         int count = 0;
         for(int number : arr){
-            if(countDigit(number)%2 == 0){
-                count++;
+            if((countDigit2(number) & 1) == 0){ //To optimized way of checking even, "(countDigit2(number & 1) == 0".
+                count++;                    //It will be "true" if even.
             }
         }
 
