ea276fcaea768bb686bb12e0a280d4b08319bf87
Vivek Kumar <vivekkr0311@gmail.com>
2023-11-30 19:00:04 +0530

    Iteration: Bubble sort flag add

--- src/com/Vivek/Iteration/Sorting/BubbleSort/BubbleSort.java
+++ src/com/Vivek/Iteration/Sorting/BubbleSort/BubbleSort.java
@@ -6,13 +6,21 @@ import java.util.Scanner;
 public class BubbleSort {
 
     static void bubble(int []arr, int n){
+        boolean swapped;
+
         for(int i = 0; i < n; i++){
+            swapped = false;
             for(int j = 1; j < n - i; j++){
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    swapped = true;
                 }
+            }
+
+            if(!swapped){
+                break;
             }
         }
     }
