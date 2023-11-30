9f6a928e0753944ddfefdde53b1b6131f1fa8387
Vivek Kumar <vivekkr0311@gmail.com>
2023-11-30 18:54:41 +0530

    Iteration: Bubble sort correction

--- src/com/Vivek/Iteration/Sorting/BubbleSort/BubbleSort.java
+++ src/com/Vivek/Iteration/Sorting/BubbleSort/BubbleSort.java
@@ -7,11 +7,11 @@ public class BubbleSort {
 
     static void bubble(int []arr, int n){
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            for(int j = 1; j < n - i; j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
         }
