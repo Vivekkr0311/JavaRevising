e028c9bfe062f120eed363354f1284631f39195c
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-20 16:43:52 +0530

    Recursion: String permutations, returning array

---
+++ src/com/Vivek/Recursion/SimpleProblemsUsingRecursion/Permutations/ReturningArrayStringPermutations.java
@@ -0,0 +1,37 @@
+package com.Vivek.Recursion.SimpleProblemsUsingRecursion.Permutations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReturningArrayStringPermutations {
+
+    static ArrayList<String> stringArrayPermutations(String processed, String unprocessed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        char ch = unprocessed.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+
+        for(int i = 0; i <= processed.length(); i++){
+            String first = processed.substring(0, i);
+            String second = processed.substring(i);
+            ans.addAll(stringArrayPermutations(first + ch + second, unprocessed.substring(1)));
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String inputString = input.nextLine();
+
+        System.out.println("Permutations of " +  inputString +" are ");
+        System.out.println(stringArrayPermutations("", inputString));
+
+    }
+}
