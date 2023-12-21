a61919bafa920c3b12c2671a827f6d3ac972c33f
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-21 14:24:34 +0530

    Recursion: Phone pad solution correction

--- src/com/Vivek/Recursion/SimpleProblemsUsingRecursion/Permutations/PhoneNumberPermutations.java
+++ src/com/Vivek/Recursion/SimpleProblemsUsingRecursion/Permutations/PhoneNumberPermutations.java
@@ -4,35 +4,26 @@ import java.util.Scanner;
 
 public class PhoneNumberPermutations {
 
-    static void permutations(String p, String up){
+    static void phonePad(String p, String up){
         if(up.isEmpty()){
             System.out.println(p);
             return;
         }
 
-        char ch = up.charAt(0);
+        int digit = up.charAt(0) - '0';
 
-        for(int i = 0; i <=  p.length(); i++){
-            String first = p.substring(0, i);
-            String second = p.substring(i, p.length());
-            permutations(first + ch + second, up.substring(1));
+        for(int i = (digit - 1) * 3; i < digit * 3; i++){
+            char ch = (char)('a' + i);
+            phonePad(p + ch, up.substring(1));
         }
     }
 
-    static void printPhoneNumberPermutations(int n){
-        String[] numberTable = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-        permutations("", numberTable[n - 1]);
-    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter phone number: \nChose from below table:\nabc: 2 \ndef: 3\nghi: 4\njkl: 5\nmno:6\npqrs:7\ntuv: 8\nwxyz: 9\n:");
-        int number = input.nextInt();
-        if(number < 2 && number > 9){
-            System.out.println("Please enter number with [2, 9]");
-        }
+        System.out.println("Enter key combination: ");
+        String keyCombination = input.nextLine();
 
-        printPhoneNumberPermutations(number);
+        phonePad("", keyCombination);
     }
 }
