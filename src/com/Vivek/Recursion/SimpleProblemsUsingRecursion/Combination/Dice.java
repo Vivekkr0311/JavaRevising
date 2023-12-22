2c99836bff7cf3eaac59652ee2dc44e950f55a3f
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-22 17:11:19 +0530

    Recursion: Dice problem add

---
+++ src/com/Vivek/Recursion/SimpleProblemsUsingRecursion/Combination/Dice.java
@@ -0,0 +1,24 @@
+package com.Vivek.Recursion.SimpleProblemsUsingRecursion.Combination;
+
+import java.util.Scanner;
+
+public class Dice {
+
+    static void dice(String p, int target){
+        if(target == 0){
+            System.out.println(p);
+            return;
+        }
+
+        for(int i = 1; i <= 6 && i <= target; i++){
+            dice(p + i, target -i);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your number which you want to make using dice: ");
+        int num = input.nextInt();
+
+        dice("", num);
+    }
+}
