d48e524d0f9849bcb6a94d7f31b3364455480ee4
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-14 16:47:38 +0530

    Obstacle Maze: Logic fix

--- src/com/Vivek/BackTracking/ObstacleInThePath.java
+++ src/com/Vivek/BackTracking/ObstacleInThePath.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ObstacleInThePath {
 
     static int obstacleInThePath(int r, int c){
-        if(r == 1 || c == 1){
+        if(r == 1 && c == 1){
             return 1;
         }
 
@@ -13,10 +13,25 @@ public class ObstacleInThePath {
         if(r == 2 && c == 2){
             return 0;
         }
-
-        int left = obstacleInThePath(r - 1, c);
-        int right = obstacleInThePath(r, c -1);
+        int left = 0; int right = 0;
+        if(r > 0) left = obstacleInThePath(r - 1, c);
+        if(c > 0) right = obstacleInThePath(r, c -1);
         return left + right;
+    }
+
+    static void obstacleInThePathPrint(String path, int r, int c){
+        if(r == 1 && c == 1){
+            System.out.println(path + " End");
+            return;
+        }
+
+        //Mimicing obstacle is at r == 2 and c == 2
+        if(r == 2 && c == 2){
+            return;
+        }
+
+        if(r > 1) obstacleInThePathPrint(path + "Down -> ", r - 1, c);
+        if(c > 1) obstacleInThePathPrint(path + "Right -> ", r, c -1);
     }
 
     public static void main(String[] args) {
@@ -25,5 +40,7 @@ public class ObstacleInThePath {
         int r = input.nextInt(); int c = input.nextInt();
 
         System.out.println("Number of paths: " + obstacleInThePath(r, c));
+        System.out.println("Paths are: ");
+        obstacleInThePathPrint("", r, c);
     }
 }
