28f70b654feb490ee83205e374934693cdc63a6d
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-14 17:27:05 +0530

    Obstacle Maze: Another solution add

---
+++ src/com/Vivek/BackTracking/ObstacleMazeAnotherSolution.java
@@ -0,0 +1,29 @@
+package com.Vivek.BackTracking;
+
+import java.util.Scanner;
+
+public class ObstacleMazeAnotherSolution {
+
+    static void obstaclePath(String path, boolean[][] maze, int r, int c){
+        if(r == maze.length -1 && c == maze[0].length - 1){
+            System.out.println(path);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r < maze.length - 1) obstaclePath(path + "D", maze, r + 1, c);
+        if(c < maze[0].length -1) obstaclePath(path + "R", maze, r, c + 1);
+    }
+
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true},
+        };
+        obstaclePath("", board, 0, 0);
+    }
+}
