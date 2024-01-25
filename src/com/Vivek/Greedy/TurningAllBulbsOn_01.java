97d8f0220a7b39b0d54c410a01ad660f532bd2f3
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-25 18:06:50 +0530

    Greedy: Naive solution fix

--- src/com/Vivek/Greedy/TurningAllBulbsOn_01.java
+++ src/com/Vivek/Greedy/TurningAllBulbsOn_01.java
@@ -14,39 +14,21 @@ public class TurningAllBulbsOn_01 {
         }
     }
 
-    static int calculatinCost(int[] arr, int start){
+    static int calculatinCost(int[] arr){
         int cost = 0;
-        for(int i = start; i < arr.length; i++){
+        for(int i = 0; i < arr.length; i++){
             if(arr[i] == 0){
                 arr[i] = 1;
                 switchRigthBulbs(arr, i + 1);
                 System.out.println(Arrays.toString(arr));
                 cost = cost + 1;
-            }else{
-                break;
             }
         }
         return cost;
     }
 
-    static int solution(int[] arr){
-        int[] allcost = new int[arr.length];
-        for(int i = 0; i < arr.length; i++){
-            int currCost =  calculatinCost(arr, i);
-            allcost[i] = currCost;
-        }
-
-        int maxCost = allcost[0];
-        for(int i = 1; i < allcost.length; i++){
-            if(allcost[i] > maxCost){
-                maxCost = allcost[i];
-            }
-        }
-        return maxCost;
-    }
-
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 1, 1, 0, 1, 1};
-        System.out.println(solution(arr));
+        System.out.println(calculatinCost(arr));
     }
 }
---
+++ src/com/Vivek/Greedy/TurningAllBulbsOn_02.java
@@ -0,0 +1,27 @@
+package com.Vivek.Greedy;
+
+public class TurningAllBulbsOn_02 {
+
+    static int bulbs(int[] arr){
+        int cost = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]%2 == 0){
+                // nothing needs to be change
+            }else{
+                arr[i] = (arr[i] == 0) ? 1 : 0;
+            }
+
+            if(arr[i]%2 == 1){
+                continue;
+            }else{
+                cost++;
+            }
+        }
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 1, 1, 0, 1, 1};
+        System.out.println(bulbs(arr));
+    }
+}
