03a04ed8db38841f9cf4fe9d05d04a754174fd65
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-28 16:33:34 +0530

    Greedy: Seating together optimal solution

--- src/com/Vivek/Greedy/Seats_07.java
+++ src/com/Vivek/Greedy/Seats_07.java
@@ -7,6 +7,42 @@ import java.util.Scanner;
 
 public class Seats_07 {
 
+    // Most optimal solution
+    public static void seatsThree(String input){
+        List<Integer> allXIdx = new ArrayList<>();
+        // This loop is storing the indexes of all 'x's
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == 'x'){
+                allXIdx.add(i);
+            }
+        }
+        System.out.println("All idx of x: " + allXIdx);
+        int n = allXIdx.size();
+        if(n == 0){
+            return;
+        }
+
+        List<Integer> distanceX = new ArrayList<>();
+        // This loop is calulating the distance of the 'x's considering the segment start from the idx 0.
+        for(int i = 0; i < allXIdx.size(); i++){
+            distanceX.add(allXIdx.get(i) - i);
+        }
+
+        System.out.println("Distance of each x's: " + distanceX);
+
+        // We are assigning it to very large number as we need to find the minimum.
+        int ans = Integer.MAX_VALUE;
+        // Now we are using median of distances of x's
+        int segmentStart = distanceX.get((distanceX.size() / 2));
+        int total = 0;
+        for(int idx = 0; idx < distanceX.size(); idx++){
+            total = total + Math.abs(distanceX.get(idx) - segmentStart);
+        }
+        ans = Math.min(ans, total);
+
+        System.out.println(ans);
+    }
+
     public static void seatsTwo(String input){
         List<Integer> allXIdx = new ArrayList<>();
         // This loop is storing the indexes of all 'x's
@@ -78,6 +114,7 @@ public class Seats_07 {
 
         int ans = seats(in);
         System.out.println(ans);
-        seatsTwo(in);
+//        seatsTwo(in);
+        seatsThree(in);
     }
 }
