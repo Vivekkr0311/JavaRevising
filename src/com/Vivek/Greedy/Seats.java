1a73ebca8d5cfa5c75e5a0e628dd631a9a82f572
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-30 13:53:37 +0530

    Greedy: Files name changed

--- src/com/Vivek/Greedy/Seats.java
+++ src/com/Vivek/Greedy/Seats.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Seats_07 {
+public class Seats {
 
     // Most optimal solution
     public static void seatsThree(String input){
