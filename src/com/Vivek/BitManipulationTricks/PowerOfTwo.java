82948fcf021e95d6a50fd51448371ef4469752bd
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-30 12:30:18 +0530

    Bit Manipulation: Check power of 2

---
+++ src/com/Vivek/BitManipulationTricks/PowerOfTwo.java
@@ -0,0 +1,21 @@
+package com.Vivek.BitManipulationTricks;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+
+    static boolean isPowerOfTwo(int num){
+        if(num == 0){
+            return false;
+        }
+        return (num & (num - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number to check if it is power of 2: ");
+        int num = input.nextInt();
+
+        System.out.println(num + " is power of 2 ?: " + isPowerOfTwo(num));
+    }
+}
