7b8493283beb7c78184cf6b3bb60fddce8de5285
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-30 14:07:32 +0530

    Bit Manipulation: Toggle a bit

---
+++ src/com/Vivek/BitManipulationTricks/ToggleBit.java
@@ -0,0 +1,24 @@
+package com.Vivek.BitManipulationTricks;
+
+import java.util.Scanner;
+
+public class ToggleBit {
+
+    static int toggleBit(int num, int k){
+        return num ^ (1 << (k - 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number whose kth bit you wanna change: ");
+        int num = input.nextInt();
+        int len = Integer.toBinaryString(num).length();
+        System.out.println("You " + num + " in binary: " + Integer.toBinaryString(num));
+        System.out.println("Enter the kth bit you wanna change (LSB): ");
+        int k = input.nextInt();
+
+        System.out.println("After toggling " + k + "th bit");
+        String binaryString = Integer.toBinaryString(toggleBit(num, k));
+        System.out.println(String.format("%" + len + "s", binaryString).replace(' ', '0'));
+    }
+}
