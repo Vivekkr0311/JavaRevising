8f36da472d9764975a149b7625a3eee7ca6b8f42
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-03 16:54:05 +0530

    Greedy: Valid Palindrome efficient code

--- src/com/Vivek/Greedy/ValidPalindromeTwo.java
+++ src/com/Vivek/Greedy/ValidPalindromeTwo.java
@@ -16,17 +16,31 @@ public class ValidPalindromeTwo {
         return true;
     }
 
+    // Efficient
     static boolean isValid(String s, int start, int end, int deletion){
-        if(start > end){
-            return true;
+        if(deletion < 0){
+            return false;
+        }
+
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end)){
+                return isValid(s, start + 1, end, deletion - 1) || isValid(s, start, end - 1, deletion - 1);
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    static boolean isValidTwo(String s, int start, int end, int deletion){
+        if(deletion < 0){
+            return false;
         }
 
         if(s.charAt(start) == s.charAt(end)){
-            return isValid(s, start + 1, end - 1, deletion);
+            return isValidTwo(s, start + 1, end - 1, deletion);
         }
-        boolean forwardTrim = deletion != 1 && isValid(s, start + 1, end, deletion + 1);
-        boolean backwardTrim = deletion != 1 && isValid(s, start, end - 1, deletion + 1);
-        return deletion == 0 &&  backwardTrim || forwardTrim;
+        return isValidTwo(s, start + 1, end, deletion - 1) || isValidTwo(s,start, end - 1, deletion - 1);
     }
 
     public static void main(String[] args) {
