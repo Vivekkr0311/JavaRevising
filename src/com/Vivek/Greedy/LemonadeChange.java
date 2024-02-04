bb85e2c49dcbc6b75db6228840086735647d441d
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-04 12:11:13 +0530

    Greedy: Lemonade change

--- src/com/Vivek/Greedy/LemonadeChange.java
+++ src/com/Vivek/Greedy/LemonadeChange.java
@@ -4,29 +4,73 @@ import java.util.Scanner;
 
 public class LemonadeChange {
 
-    static boolean lemonadeChange(int[] bills){
-        final int eachBuy = 5;
+//    static boolean changeAvailable(int[] bills, int change, int i){
+//        int amount = change;
+//        for(int k = i; k >= 0 && amount > 0; k--){
+//            if(bills[k] == amount){
+////                amount -= bills[i];
+//                bills[k] = 0;
+//                return true;
+//            }else if(bills[k] < amount){
+//                amount -=  bills[k];
+//                bills[k] = 0;
+//            }
+//        }
+//
+//        return amount == 0 ? true : false;
+//    }
 
-        int collectedChange = 0;
+//    static boolean lemonadeChange(int[] bills){
+//        final int eachBuy = 5;
+//
+//        for(int i = 0; i < bills.length; i++){
+//            int remainder = bills[i] - eachBuy;
+//            if(remainder == 0){
+//                continue;
+//            }else{
+//                // search for remainder in array i - 1 down to 0
+//                boolean flag = changeAvailable(bills, remainder, i - 1);
+//                if(!flag){
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+
+    static boolean lemonadeChangeSolution(int[] bills){
+        int fives = 0;
+        int tens = 0;
+        int twenty = 0;
+
         for(int i = 0; i < bills.length; i++){
-            if(bills[i] == eachBuy){
-                collectedChange += bills[i];
-                continue;
+            if(bills[i] == 5){
+                fives++;
+            }else if(bills[i] == 10){
+                fives--;
+                tens++;
+            }else if(bills[i] == 20){
+                if(tens > 0){
+                    tens--;
+                    fives--;
+                    twenty++;
+                }else{
+                    fives -= 3;
+                }
             }
-            if(collectedChange < bills[i] - eachBuy){
-                return false;
-            }
-            int remainder = bills[i] - eachBuy;
-            collectedChange -= remainder;
-            collectedChange += eachBuy;
+            if(fives < 0) return false;
         }
         return true;
     }
+
 
     public static void main(String[] args) {
 
         // Test case:
         // [5,5,5,5,20,20,5,5,20,5]
+        // [5, 5, 5, 10, 20]: true
+        // [5, 5, 10, 10, 20]: false
+        // 5 10 20 5: false
         Scanner input = new Scanner(System.in);
         System.out.println("How many customers? ");
         int n = input.nextInt();
@@ -36,6 +80,6 @@ public class LemonadeChange {
             bills[i] = input.nextInt();
         }
 
-        System.out.println(lemonadeChange(bills));
+        System.out.println(lemonadeChangeSolution(bills));
     }
 }
