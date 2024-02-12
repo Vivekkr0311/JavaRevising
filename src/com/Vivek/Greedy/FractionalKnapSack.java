d3e215314883cc06122628c5a40238985616b501
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-12 10:20:48 +0530

    Greedy: Fractional Knapsack Value over Weight ratio

--- src/com/Vivek/Greedy/FractionalKnapSack.java
+++ src/com/Vivek/Greedy/FractionalKnapSack.java
@@ -35,6 +35,13 @@ public class FractionalKnapSack {
         }
     }
 
+    static class MyCmpProfitOverWeight implements Comparator<Item>{
+        @Override
+        public int compare(Item a1, Item a2){
+            return -((a1.value/a1.weight) - (a2.value/a2.weight));
+        }
+    }
+
     public static double fractionalKnapSackGreedyAboutValues(Item[] items, int capacity){
         int count = 1;
         Arrays.sort(items, new MyCmpValues());
@@ -85,6 +92,38 @@ public class FractionalKnapSack {
         return count + (double)capacity / items[lastIdx].weight;
     }
 
+    public static double fractionalKnapSackGreedyAboutValueOverWeight(Item[] items, int capacity){
+        int count = 1;
+//        double[] profitOverWeight = new double[items.length];
+//
+//        for(int i = 0; i < items.length; i++){
+//            profitOverWeight[i] = -(double) items[i].value / items[i].weight;
+//        }
+
+        Arrays.sort(items, new MyCmpProfitOverWeight());
+
+        System.out.println(Arrays.toString(items));
+        int lastIdx = 0;
+        int totalProfit = items[0].value;
+        capacity -= items[0].weight;
+        for(int i = 1; i < items.length; i++){
+            if(capacity >= 0 && capacity >= items[i].weight){
+                totalProfit += items[i].value;
+                capacity -= items[i].weight;
+                count++;
+            }
+            lastIdx = i;
+            if(capacity <= 0){
+                break;
+            }
+        }
+        if(capacity >= 0){
+            totalProfit += items[lastIdx].value * ((double)capacity / items[lastIdx].weight);
+        }
+        System.out.println("Total Profit: " + totalProfit);
+        return count + (double)capacity / items[lastIdx].weight;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("How many item do you have ?");
@@ -103,5 +142,7 @@ public class FractionalKnapSack {
         System.out.println("Maximum item that you can have when greedy about values/ profit: " + result);
         double resultTwo = fractionalKnapSackGreedyAboutSmallestWeight(items, capacity);
         System.out.println("Maximum item that you can have when greedy about having item with smallest weight: " + resultTwo);
+        double resultThree = fractionalKnapSackGreedyAboutValueOverWeight(items, capacity);
+        System.out.println("Maximum item that you can have when greedy about value over weight: " + resultThree);
     }
 }
