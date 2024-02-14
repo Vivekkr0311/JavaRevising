f2c659be9f036c291eb7b6d70684d50504d2d2b6
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-14 20:15:32 +0530

    Collections: k largest element efficient solution, PriorityQueue

--- src/com/Vivek/Collections/PriorityQueue/K_LargestElements.java
+++ src/com/Vivek/Collections/PriorityQueue/K_LargestElements.java
@@ -1,5 +1,6 @@
 package com.Vivek.Collections.PriorityQueue;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -18,19 +19,43 @@ public class K_LargestElements {
         }
     }
 
+    static void kLargestElementsEfficient(Integer[] arr, int k){
+        PriorityQueue<Integer> minHeap= new PriorityQueue<>();
+        for(int i = 0; i < k; i++){
+            minHeap.add(arr[i]);
+        }
+
+        for(int i = k; i < arr.length; i++){
+            if(minHeap.peek() < arr[i]){
+                minHeap.poll();
+                minHeap.add(arr[i]);
+            }
+        }
+
+        while(!minHeap.isEmpty()){
+            System.out.print(minHeap.poll() + " ");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter how many element you have: ");
         int n = input.nextInt();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Integer[] arr = new Integer[n]; // For only efficient solution.
         System.out.println("Enter each element: ");
         for(int i = 0; i < n; i++){
-            maxHeap.add(input.nextInt());
+            int temp = input.nextInt();
+            maxHeap.add(temp);
+            arr[i] = temp;
         }
 
         System.out.println("Enter how many largest element you want (k): ");
         int k = input.nextInt();
         System.out.print("K Largest element are: ");
         kLargestElements(maxHeap, k);
+        System.out.println();
+        System.out.print("K Largest element are: ");
+        kLargestElementsEfficient(arr, k);
     }
 }
