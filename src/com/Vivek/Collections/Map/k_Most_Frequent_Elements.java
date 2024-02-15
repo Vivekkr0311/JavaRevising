3b98ca1cbb3a682ae6003ddd1257b0b9ffbc1b95
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-15 12:42:58 +0530

    Collections Map: k most frequent element solution two

--- src/com/Vivek/Collections/Map/k_Most_Frequent_Elements.java
+++ src/com/Vivek/Collections/Map/k_Most_Frequent_Elements.java
@@ -4,6 +4,27 @@ import java.util.*;
 
 public class k_Most_Frequent_Elements {
 
+    public static class MyCmp implements Comparator<Map.Entry<Integer, Integer>> {
+        @Override
+        public int compare(Map.Entry<Integer, Integer> m1, Map.Entry<Integer, Integer> m2){
+            return -(m1.getValue() - m2.getValue());
+        }
+    }
+
+    static void kMostFrequentElementTwo(Integer[] arr, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 1) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, new MyCmp());
+
+        for(int i = 0; i < k; i++){
+            System.out.println(list.get(i) + " ");
+        }
+    }
+
     static void kMostFrequentElement(Integer[] arr, int k){
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < arr.length; i++){
@@ -17,6 +38,7 @@ public class k_Most_Frequent_Elements {
             System.out.println(list.get(i) + " ");
         }
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("How many element do you have? ");
@@ -29,5 +51,7 @@ public class k_Most_Frequent_Elements {
         System.out.println("Enter value of 'k', to take k most frequent elements: ");
         int k = input.nextInt();
         kMostFrequentElement(arr, k);
+        System.out.println();
+        kMostFrequentElementTwo(arr, k);
     }
 }
