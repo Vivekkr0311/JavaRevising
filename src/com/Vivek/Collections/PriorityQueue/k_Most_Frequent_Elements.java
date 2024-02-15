8c01d7889809b91a0c3a3f597f3d8472fce4f0f7
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-15 13:33:40 +0530

    Collection PriorityQueue: k most frequent elements

--- src/com/Vivek/Collections/PriorityQueue/k_Most_Frequent_Elements.java
+++ src/com/Vivek/Collections/PriorityQueue/k_Most_Frequent_Elements.java
@@ -23,7 +23,37 @@ public class k_Most_Frequent_Elements {
             k--;
         }
     }
+
+    public static class MyCmp implements Comparator<Map.Entry<Integer, Integer>> {
+        @Override
+        public int compare(Map.Entry<Integer, Integer> m1, Map.Entry<Integer, Integer> m2){
+            if(m1.getValue() ==  m2.getValue()){
+                return m1.getKey() - m2.getKey();
+            }else{
+                return -(m1.getValue() - m2.getValue());
+            }
+        }
+    }
+
+    static void kMostFrequenctEfficient(Integer[] arr, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 1) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(new MyCmp());
+        for(Map.Entry<Integer, Integer> m: map.entrySet()){
+            maxHeap.add(m);
+        }
+
+        for(int i = 0; i < k; i++){
+            System.out.println(maxHeap.poll());
+        }
+    }
+
     public static void main(String[] args) {
+        // input: 10 5 20 5 10 10 30, n = 7, k=2
+        // output: 10=4, 5=3
         Scanner input = new Scanner(System.in);
         System.out.println("How many element do you have? ");
         int n = input.nextInt();
@@ -35,5 +65,7 @@ public class k_Most_Frequent_Elements {
         System.out.println("Enter value of 'k', to take k most frequent elements: ");
         int k = input.nextInt();
         kMostFrequentElement(arr, k);
+        System.out.println();
+        kMostFrequenctEfficient(arr, k);
     }
 }
