d4178a45b88cfef54eba67ad33f9cdd4b2a52553
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-22 18:16:34 +0530

    Greedy: Huffman Coding work in-progress

---
+++ src/com/Vivek/Greedy/HuffmanCoding.java
@@ -0,0 +1,32 @@
+package com.Vivek.Greedy;
+
+import java.util.HashMap;
+
+class HumanCoder {
+    HashMap<Character, String> encoder;
+    HashMap<Character, String> decoder;
+
+    private class Node implements Comparable <Node>{
+        Character data;
+        int cost;
+        Node left;
+        Node right;
+
+        public Node(Character data, int cost){
+            this.data = data;
+            this.cost = cost;
+            this.left = null;
+            this.right = null;
+        }
+
+        @
+    }
+}
+public class HuffmanCoding {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
