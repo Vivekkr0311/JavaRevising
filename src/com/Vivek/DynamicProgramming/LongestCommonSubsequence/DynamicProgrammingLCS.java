97367dbe7ee1016a63a4c7e856fb8a22c357ef99
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-25 13:29:37 +0530

    Dynamic Programming: LCS memoization code add:

--- src/com/Vivek/DynamicProgramming/LongestCommonSubsequence/DynamicProgrammingLCS.java
+++ src/com/Vivek/DynamicProgramming/LongestCommonSubsequence/DynamicProgrammingLCS.java
@@ -5,7 +5,31 @@ import java.util.Scanner;
 
 public class DynamicProgrammingLCS {
 
+    static int[][] memo;
+
+    static int dynamicProgrammingLCSMemo(String s1, String s2, int m, int n){
+        if(memo[m][n] != -1){
+            return memo[m][n];
+        }
+
+        if(m == 0 || n == 0){
+            return memo[m][n];
+        }else{
+            if(s1.charAt(m - 1) == s2.charAt(n - 1)){
+                memo[m][n] = 1 + dynamicProgrammingLCSMemo(s1, s2, m - 1, n -1);
+            }else{
+                memo[m][n] = Math.max(
+                        dynamicProgrammingLCSMemo(s1, s2, m, n - 1),
+                        dynamicProgrammingLCSMemo(s1, s2, m - 1, n)
+                );
+            }
+        }
+        return memo[m][n];
+    }
+
+
     static int dynamicProgrammingLCS(String s1, String s2){
+        // This is a tablulation solution.
         int m = s1.length();
         int n = s2.length();
 
@@ -39,6 +63,8 @@ public class DynamicProgrammingLCS {
         String ip = input.nextLine();
         System.out.println("Enter your second string to find its LCS: ");
         String ip2 = input.nextLine();
+        Arrays.fill(memo, -1);
         System.out.println(dynamicProgrammingLCS(ip, ip2));
+        System.out.println(dynamicProgrammingLCSMemo(ip, ip2, ip.length(), ip2.length()));
     }
 }
