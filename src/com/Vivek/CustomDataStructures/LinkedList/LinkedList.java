26cfab3a80a56f3af601384b1b350ed56e2514de
Vivek Kumar <vivekkr0311@gmail.com>
2024-02-29 14:43:00 +0530

    Custome Data Structures: Linked list insert at idx

--- src/com/Vivek/CustomDataStructures/LinkedList/LinkedList.java
+++ src/com/Vivek/CustomDataStructures/LinkedList/LinkedList.java
@@ -28,11 +28,45 @@ public class LinkedList {
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
-        size++;
+        this.size++;
 
         if(tail == null){
             tail = head;
         }
+    }
+
+    public void insertEnd(int data){
+        Node newNode = new Node(data);
+        if(tail == null){
+            insertFirst(data);
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+        this.size++;
+    }
+
+    public void insertAt(int data, int idx){
+        if(idx > size){
+            System.out.println("cannot insert at " + idx);
+            System.out.println("List size is " + getSize());
+            return;
+        }
+        if(idx == 0){
+            insertFirst(data);
+            return;
+        }
+        if(idx == size){
+            insertEnd(data);
+            return;
+        }
+        Node temp = head;
+        for(int i = 1; i < idx; i++){
+            temp = temp.next;
+        }
+        Node newNode = new Node(data, temp.next);
+        temp.next = newNode;
+        size++;
     }
 
     public void display(){
@@ -42,5 +76,9 @@ public class LinkedList {
             temp = temp.next;
         }
         System.out.println(" END");
+    }
+
+    public int getSize(){
+        return this.size;
     }
 }
--- src/com/Vivek/CustomDataStructures/LinkedList/Main.java
+++ src/com/Vivek/CustomDataStructures/LinkedList/Main.java
@@ -3,14 +3,16 @@ package com.Vivek.CustomDataStructures.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.insertFirst(1);
+        list.insertEnd(1);
         list.insertFirst(6);
         list.insertFirst(7);
-        list.insertFirst(8);
-        list.insertFirst(2);
-        list.insertFirst(9);
-        list.insertFirst(4);
-
+        list.insertEnd(8);
         list.display();
+        list.insertAt(100, 3);
+        list.display();
+        list.insertAt(200, 1);
+        list.display();
+        list.insertAt(4, 464);
+        System.out.println(list.getSize());
     }
 }
