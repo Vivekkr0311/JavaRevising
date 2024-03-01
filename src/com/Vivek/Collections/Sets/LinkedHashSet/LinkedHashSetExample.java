8dc6214fee2c3344582cbf10c53cf211b1a019e0
Vivek Kumar <vivekkr0311@gmail.com>
2024-03-01 10:57:52 +0530

    Java Collections: LinkedHashSet

--- src/com/Vivek/Collections/Sets/LinkedHashSet/LinkedHashSetExample.java
+++ src/com/Vivek/Collections/Sets/LinkedHashSet/LinkedHashSetExample.java
@@ -9,6 +9,10 @@ public class LinkedHashSetExample {
         // Which is why, it is able to maintain the order in which the items are inserted.
         // This is the only advantage over HashSet.
 
+        // Time complexity is same as HashSet:
+        // add(), contains() are O(1) operation on average.
+        // size(), isEmpty() are O(1) operation.
+
         LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
         System.out.println(linkedHashSet.add(10));
         System.out.println(linkedHashSet.add(20));
