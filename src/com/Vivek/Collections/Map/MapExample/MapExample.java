63d71331b96c8788e775f94149fd028558e8cfc9
Vivek Kumar <vivekkr0311@gmail.com>
2024-03-01 17:35:24 +0530

    Java Collections: HashMap example

--- src/com/Vivek/Collections/Map/MapExample/MapExample.java
+++ src/com/Vivek/Collections/Map/MapExample/MapExample.java
@@ -12,14 +12,20 @@ public class MapExample {
         // HashMap and LinkedHashMap, uses Hashing internally which allows us quickly insert, search and delete.
         // While TreeMap, take O(log(n)) for search, delete and insert, but it provides sorted data access.
         // And TreeMap also have methods like ceiling(), floor(), lower(), higher() which are not present in HashMap or LinkedHashMap.
-        
+
 
         Map<Integer, String> map = new TreeMap<>();
 
-        map.put(1, "Vivek");
+        map.put(3, "Vivek Three");
         map.put(2, "Vivek Two");
-        map.put(3, "Vivek three");
+        map.put(1, "ABC");
+        map.put(1, "Vivek"); // It will replace the value associated with the give key 1.
+
 
         System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
+
     }
 }
