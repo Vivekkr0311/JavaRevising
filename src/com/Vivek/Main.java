848510100b5adaacef7a6453d28fe8cf919220ab
Vivek Kumar <vivekkr0311@gmail.com>
2024-03-04 19:37:32 +0530

    String objects: Main class modified

--- src/com/Vivek/Main.java
+++ src/com/Vivek/Main.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input =  new Scanner(System.in);
-        Scanner input2 = new Scanner(System.in);
-        System.out.println("What is your name?? \n");
-        String name = input.nextLine();
-        System.out.println("What is your dob?? \n");
-        int dob = input2.nextInt();
+        String name = "Vivek";
+        String nameTwo = "Vivek";
+        // Both are refering to the same String object.
+        System.out.println(name == nameTwo);
+        
+        //Now a new String is object is created therefore now
+        //both variable will be refering to two different String variables.
+        nameTwo = new String("Vivek");
+        System.out.println(name == nameTwo);
 
-        if((dob == 1) && name.equals("Jelly")){
-            System.out.println("Happy Birthday " + name);
-        }else{
-            System.out.println("Sirf jelly ke liye hai ye computer program.");
-        }
+
+        // This will not change the actual String object, instead it will create a new String object.
+        name = name.substring(0, 4) + 'z';
+        System.out.println(name);
     }
 }
