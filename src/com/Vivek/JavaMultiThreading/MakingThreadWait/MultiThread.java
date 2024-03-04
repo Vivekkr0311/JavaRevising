113363a86b3e2bc0d4d99066d0834223c9e24e5c
Vivek Kumar <vivekkr0311@gmail.com>
2024-03-04 19:19:21 +0530

    MultiThread: Making thread join other thread

---
+++ src/com/Vivek/JavaMultiThreading/MakingThreadWait/MultiThread.java
@@ -0,0 +1,21 @@
+package com.Vivek.JavaMultiThreading.MakingThreadWait;
+
+public class MultiThread implements Runnable{
+    private int threadNumber;
+
+    MultiThread(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i + " from thread number " + this.threadNumber);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
