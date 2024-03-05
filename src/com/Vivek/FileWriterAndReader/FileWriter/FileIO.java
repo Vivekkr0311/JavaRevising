9ee8d9c8de726205fc65ae62baa4ed96a6a41e39
Vivek Kumar <vivekkr0311@gmail.com>
2024-03-05 18:03:12 +0530

    File IO: File Writer

---
+++ src/com/Vivek/FileWriterAndReader/FileWriter/FileIO.java
@@ -0,0 +1,17 @@
+package com.Vivek.FileWriterAndReader.FileWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+public class FileIO {
+    public static void main(String[] args) {
+        try {
+            String currentDir = System.getProperty("user.dir");
+            String fullPath = currentDir + "/src/com/Vivek/FileWriterAndReader/FileWriter/output.txt";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fullPath));
+            writer.write("WRITING TO THE FILE!!");
+            writer.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
