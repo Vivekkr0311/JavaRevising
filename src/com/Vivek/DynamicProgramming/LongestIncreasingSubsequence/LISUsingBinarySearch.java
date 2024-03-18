3f082bc846907304f3ff75e40484a13b11c4b15d
Vivek Kumar <vivekkr0311@gmail.com>
2024-03-18 22:45:46 +0530

    Dynamic Programming: LIS using Binary search logic fix

--- src/com/Vivek/DynamicProgramming/LongestIncreasingSubsequence/LISUsingBinarySearch.java
+++ src/com/Vivek/DynamicProgramming/LongestIncreasingSubsequence/LISUsingBinarySearch.java
@@ -1,5 +1,6 @@
 package com.Vivek.DynamicProgramming.LongestIncreasingSubsequence;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,11 +8,11 @@ import java.util.List;
 public class LISUsingBinarySearch {
 
     // This solution take O(n * log(n)) time
-    static int ceil(int[] arr, int start, int end, int num){
+    static int ceil(List<Integer> arr, int start, int end, int num){
 
         while(start < end){
             int mid = start + (end - start)/2;
-            if(arr[mid] >= num){
+            if(arr.get(mid) >= num){
                 end = mid;
             }else{
                 start = mid + 1;
@@ -24,10 +25,10 @@ public class LISUsingBinarySearch {
         List<Integer> list = new ArrayList<>();
         list.add(arr[0]);
         for(int i = 1; i < arr.length; i++){
-            if(arr[i - 1] < arr[i]){
+            if(arr[i] > list.get(list.size() - 1)){
                 list.add(arr[i]);
             }else{
-                int idx = ceil(arr, 0, i, arr[i]);
+                int idx = ceil(list, 0, list.size() - 1, arr[i]);
                 list.set(idx, arr[i]);
             }
         }
@@ -41,6 +42,9 @@ public class LISUsingBinarySearch {
 //        Enter the number of elements:
 //        7
 //        3 4 2 8 10 5 1
+//        6
+//        Enter each element:
+//        3 10 20 4 6 7
         int[] arr = new int[n];
         System.out.println("Enter each element: ");
         for(int i = 0; i < n; i++){
