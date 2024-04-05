28aca250a7de1e8dcd87812fab76d8bd14826795
Vivek Kumar <vivekkr0311@gmail.com>
2024-04-05 14:15:29 +0530

    Dynamic Programming: Egg Dropping Problem recursive code

---
+++ src/com/Vivek/DynamicProgramming/EggDropping/RecursiveSolution.java
@@ -0,0 +1,35 @@
+package com.Vivek.DynamicProgramming.EggDropping;
+
+import java.util.Scanner;
+
+public class RecursiveSolution {
+
+    static int eggDrop(int e, int f){
+        if(f == 1 || f == 0){
+            return f;
+        }
+        if(e == 1){
+            return f;
+        }
+
+        int res = Integer.MAX_VALUE;
+        for(int i = 1; i <= f; i++){
+            int sub_res = Math.max(
+                    eggDrop(e, f - i),
+                    eggDrop(e - 1, i - 1)
+            );
+            res = Math.min(res, sub_res + 1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of eggs: ");
+        int e = input.nextInt();
+        System.out.println("Enter number of floors: ");
+        int f = input.nextInt();
+
+        System.out.println("Minimum number of attempts required to find critical floor: " + eggDrop(e, f));
+    }
+}
