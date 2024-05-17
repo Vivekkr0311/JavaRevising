c4634469abaa5538aee93a35f5d1eda3b0426753
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-17 15:28:45 +0530

    Data Structures: Segment Tree

--- src/com/Vivek/DataStructures/Trees/SegmentTree/SegmentTree.java
+++ src/com/Vivek/DataStructures/Trees/SegmentTree/SegmentTree.java
@@ -16,7 +16,7 @@ public class SegmentTree {
     }
 
     Node root;
-    public SegmentTree(int[] arr, int start, int end){
+    public SegmentTree(int[] arr){
         // creating a tree using this array
         this.root = constructTree(arr, 0, arr.length - 1);
     }
@@ -37,5 +37,74 @@ public class SegmentTree {
         node.data = node.left.data + node.right.data;
 
         return node;
+    }
+
+    public int query(int queryStartIndex, int queryEndIndex){
+        return this.query(this.root, queryStartIndex, queryEndIndex);
+    }
+
+    private int query(Node node, int queryStartIndex, int queryEndIndex){
+        if(node.startInterval >= queryStartIndex && node.endInterval <= queryEndIndex){
+            // node is completely lies inside query
+            return node.data;
+        }else if(node.startInterval > queryEndIndex || node.endInterval < queryStartIndex){
+            // completely outside
+            return 0;
+        }else{
+            return this.query(node.left, queryStartIndex, queryEndIndex) + this.query(node.right, queryStartIndex, queryEndIndex);
+        }
+    }
+
+    public void update(int index, int value){
+        this.root.data = update(this.root, index, value);
+    }
+
+    private int update(Node node, int index, int value){
+        if(index >= node.startInterval && index <= node.endInterval){
+            if(index == node.startInterval && index == node.endInterval){
+                node.data = value;
+                return node.data;
+            }else{
+                int leftAns = update(node.left, index, value);
+                int rightAns = update(node.right, index, value);
+                node.data = leftAns + rightAns;
+                return node.data;
+            }
+        }
+
+        return node.data;
+    }
+
+    public void display() {
+        display(this.root);
+    }
+    private void display(Node node) {
+        String str = "";
+
+        if(node.left != null) {
+            str = str + "Interval=[" + node.left.startInterval + "-" + node.left.endInterval + "] and data: " + node.left.data + " => ";
+        } else {
+            str = str + "No left child";
+        }
+
+        // for current node
+        str = str + "Interval=[" + node.startInterval + "-" + node.endInterval + "] and data: " + node.data + " <= ";
+
+        if(node.right != null) {
+            str = str + "Interval=[" + node.right.startInterval + "-" + node.right.endInterval + "] and data: " + node.right.data;
+        } else {
+            str = str + "No right child";
+        }
+
+        System.out.println(str + '\n');
+
+        // call recursion
+        if(node.left != null) {
+            display(node.left);
+        }
+
+        if(node.right != null) {
+            display(node.right);
+        }
     }
 }
