d1385aa5cc442b1323b41cb1bab58ddca19c2b7e
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-20 13:06:05 +0530

    Data Structures: Tree, right view of a tree

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/RightSideViewTree.java
@@ -0,0 +1,48 @@
+package com.Vivek.DataStructures.Trees.BinaryTree;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+
+import java.util.*;
+
+public class RightSideViewTree {
+
+    static List<Integer> rightSideViewTwo(Node root){
+        List<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+
+            for(int i = 0; i < levelSize; i++){
+                Node curr = queue.poll();
+                if(i == levelSize - 1){
+                    result.add(curr.value);
+                }
+
+                if(curr.left != null){
+                    queue.offer(curr.left);
+                }
+                if(curr.right != null){
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        Node root = tree.getRoot();
+        tree.preetyDisplay();
+
+        System.out.println(rightSideViewTwo(root));
+
+    }
+}
