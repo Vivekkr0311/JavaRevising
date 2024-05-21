90f4f2613f1d74c97ec8d4698be4977b93b0754f
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-21 17:35:03 +0530

    Data Structurs: Tres, is same tree

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/SameTree.java
@@ -0,0 +1,40 @@
+package com.Vivek.DataStructures.Trees.BinaryTree;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class SameTree {
+
+    static boolean isSameTree(Node p, Node q){
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null && q != null){
+            return false;
+        }
+        if(p != null && q == null){
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree1 = new BinaryTree();
+        BinaryTree tree2 = new BinaryTree();
+
+        tree1.populate(scanner);
+        tree2.populate(scanner);
+
+        tree1.preetyDisplay();
+        tree2.preetyDisplay();
+
+        Node p = tree1.getRoot();
+        Node q = tree2.getRoot();
+        System.out.println("Are tree same: " + isSameTree(p, q));
+    }
+}
