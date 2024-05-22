d17f19cd7149987be41b793dce11b19fafbfb41c
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-22 14:42:59 +0530

    Data Structures: Tree, Maximum depth of a tree

--- src/com/Vivek/DataStructures/Trees/BinaryTree/DFS/MaximumDepth.java
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/DFS/MaximumDepth.java
@@ -1,2 +1,27 @@
-package com.Vivek.DataStructures.Trees.BinaryTree.DFS;public class MaximumDepth {
+package com.Vivek.DataStructures.Trees.BinaryTree.DFS;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree;
+
+import java.util.Scanner;
+
+public class MaximumDepth {
+
+    static int maximumDepth(Node root, int depth){
+        if(root == null){
+            return depth;
+        }
+
+        return Math.max(maximumDepth(root.left, depth + 1), maximumDepth(root.right, depth + 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.preetyDisplay();
+        Node root = tree.getRoot();
+
+        System.out.println("Maximum depth of the tree is: " + maximumDepth(root, 0));
+    }
 }
