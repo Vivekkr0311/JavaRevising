db6dc31b75f0f7b2fd0aff7c0e7a5b5fa67f7cc8
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-28 18:53:40 +0530

    Data Structures: Tree, Find Sum Path

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/DFS/HasPathSum.java
@@ -0,0 +1,39 @@
+package com.Vivek.DataStructures.Trees.BinaryTree.DFS;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree;
+
+import java.util.Scanner;
+
+public class HasPathSum {
+
+    static boolean hasPathSum(Node root, int targetSum){
+        if(root == null){
+            return false;
+        }
+        if(root.left == null && root.right == null && targetSum - root.value == 0){
+            return true;
+        }
+
+
+        boolean left = hasPathSum(root.left, targetSum - root.value);
+        boolean right = false;
+        if(!left){
+            right = hasPathSum(root.right, targetSum - root.value);
+        }
+
+        return left || right;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.preetyDisplay();
+
+        Node root = tree.getRoot();
+        System.out.println("Enter the sum you wanna check: ");
+        int sum = scanner.nextInt();
+        System.out.println(sum + " exist: " + hasPathSum(root, sum));
+    }
+}
