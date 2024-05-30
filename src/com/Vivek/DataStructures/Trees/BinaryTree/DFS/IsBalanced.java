99871571576d1b399b1bd02e27c7700ed9be0880
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-30 17:15:19 +0530

    Data Structures: Tree, is balanced

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/DFS/IsBalanced.java
@@ -0,0 +1,46 @@
+package com.Vivek.DataStructures.Trees.BinaryTree.DFS;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree;
+
+import java.util.Scanner;
+
+public class IsBalanced {
+
+    static int height(Node node){
+        if(node == null){
+            return 0;
+        }
+
+        if(node.left == null && node.right == null && node != null){
+            return 1;
+        }
+
+        int left = height(node.left) + 1;
+        int right = height(node.right) + 1;
+        return Math.max(left, right);
+    }
+
+    static boolean isBalanced(Node root){
+        if(root == null){
+            return true;
+        }
+
+        boolean leftCall = isBalanced(root.left);
+        boolean rightCall = isBalanced(root.right);
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return !(Math.abs(leftHeight - rightHeight) > 1) && leftCall && rightCall;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+
+        tree.preetyDisplay();
+        Node root = tree.getRoot();
+        System.out.println("Is tree balanced: " + isBalanced(root));
+    }
+}
