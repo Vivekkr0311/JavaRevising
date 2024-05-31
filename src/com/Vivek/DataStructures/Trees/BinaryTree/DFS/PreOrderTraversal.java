5d6239c23ce00aea4d9a6d9ea00b3325214621a7
Vivek Kumar <vivekkr0311@gmail.com>
2024-05-31 13:52:00 +0530

    Data Strucutures: Tree, Pre Order traversal

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/DFS/PreOrderTraversal.java
@@ -0,0 +1,35 @@
+package com.Vivek.DataStructures.Trees.BinaryTree.DFS;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+public class PreOrderTraversal {
+
+    static List<Integer> preorderTraversal(Node root){
+        if(root == null){
+            return new ArrayList<>();
+        }
+
+        List<Integer> list = new ArrayList<>();
+        list.add(root.value);
+
+        List<Integer> left = preorderTraversal(root.left);
+        List<Integer> right = preorderTraversal(root.right);
+        left.addAll(right);
+        list.addAll(left);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.preetyDisplay();
+        Node root = tree.getRoot();
+
+        System.out.println("Preorder traversal of tree is: " + preorderTraversal(root));
+    }
+}
