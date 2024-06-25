53c4520082e70fe574fd4157cc70d9d4e06e84fe
Vivek Kumar <vivekkr0311@gmail.com>
2024-06-25 18:57:32 +0530

    Tree: Connect Right Pointer 2 and Create BST

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/CreateBST.java
@@ -0,0 +1,61 @@
+package com.Vivek.DataStructures.Trees.BinaryTree;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+
+import java.util.Scanner;
+
+public class CreateBST {
+
+    static Node createTree(int[] nums, int start, int end){
+        if(start == end){
+            Node node = new Node(nums[start]);
+            return node;
+        }
+
+        if(start > end){
+            return null;
+        }
+
+        int rootIdx = (start + (end -  start)/2) + 1;
+        Node root = new Node(nums[rootIdx]);
+        root.right = createTree(nums, rootIdx + 1, end);
+        root.left = createTree(nums, start, rootIdx - 1);
+        return root;
+    }
+
+    private static void preetyDisplay(Node root){
+        preetyDisplay(root, 0);
+
+    }
+
+    private static void preetyDisplay(Node node, int level){
+        if(node == null){
+            return;
+        }
+
+        preetyDisplay(node.right, level + 1);
+
+        if(level != 0){
+            for(int i = 0; i < level - 1; i++){
+                System.out.print("|\t\t");
+            }
+            System.out.println("|-------->" + node.value);
+        }else{
+            System.out.println(node.value);
+        }
+        preetyDisplay(node.left, level + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of nodes value: ");
+        int n = scanner.nextInt();
+        int[] nodesValue = new int[n];
+        System.out.println("Enter each value: ");
+        for(int i = 0; i < n; i++){
+            nodesValue[i] = scanner.nextInt();
+        }
+        Node root = createTree(nodesValue, 0, nodesValue.length - 1);
+        preetyDisplay(root);
+    }
+}
---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/DFS/ConnectRightPointer2.java
@@ -0,0 +1,52 @@
+package com.Vivek.DataStructures.Trees.BinaryTree.DFS;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree;
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+
+import java.util.Scanner;
+
+public class ConnectRightPointer2 {
+
+    static Node connect(Node root) {
+        if(root==null){
+            return null;
+        }
+        if(root.left!=null){
+            if(root.right!=null){
+                root.left.next=root.right;
+            }
+            else{
+                root.left.next=findNext(root.next);
+            }
+        }
+        if(root.right!=null && root.next!=null){
+            root.right.next=findNext(root.next);
+        }
+        connect(root.right);
+        connect(root.left);
+        return root;
+    }
+
+    static Node findNext(Node next){
+        while(next!=null){
+            if(next.left!=null){
+                return next.left;
+            }
+            if(next.right!=null){
+                return next.right;
+            }
+            next=next.next;
+        }
+        return next;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.preetyDisplay();
+
+        Node root = tree.getRoot();
+        connect(root);
+    }
+}
