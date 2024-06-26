21db7d89d7606dfb186a21c728091af02f892240
Vivek Kumar <vivekkr0311@gmail.com>
2024-06-26 18:48:49 +0530

    Tree: BFS based question count complete tree nodes

---
+++ src/com/Vivek/DataStructures/Trees/BinaryTree/BFS/CountCompleteTreeNodes.java
@@ -0,0 +1,53 @@
+package com.Vivek.DataStructures.Trees.BinaryTree.BFS;
+
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree;
+import com.Vivek.DataStructures.Trees.BinaryTree.BinaryTree.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class CountCompleteTreeNodes {
+
+    private static int countNodes(Node root) {
+        if(root == null){
+            return 0;
+        }
+
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        int currSum = 0;
+        while(!queue.isEmpty()){
+
+            int currSize = queue.size();
+            currSum += currSize;
+            for(int i = 0; i < currSize; i++){
+                Node curr = queue.poll();
+
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+            }
+        }
+        return currSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.preetyDisplay();
+
+        Node root = tree.getRoot();
+        System.out.println("Number of nodes: " + countNodes(root));
+    }
+}
