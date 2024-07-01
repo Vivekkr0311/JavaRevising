e9ccbfc3b165134f5bbe5a28ed707dc0f7a8d894
Vivek Kumar <vivekkr0311@gmail.com>
2024-07-01 17:42:30 +0530

    Strings: Roman to Integer

---
+++ src/com/Vivek/StringQuestions/RomanToInteger.java
@@ -0,0 +1,42 @@
+package com.Vivek.StringQuestions;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romanNumerals = new HashMap<>();
+        romanNumerals.put('I', 1);
+        romanNumerals.put('V', 5);
+        romanNumerals.put('X', 10);
+        romanNumerals.put('L', 50);
+        romanNumerals.put('C', 100);
+        romanNumerals.put('D', 500);
+        romanNumerals.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int value = romanNumerals.get(s.charAt(i));
+            if (value < prevValue) {
+                total -= value;
+            } else {
+                total += value;
+            }
+            prevValue = value;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your roman number: ");
+        String roman = scanner.nextLine();
+
+        System.out.println(roman + " in integer is " + romanToInt(roman));
+    }
+}
