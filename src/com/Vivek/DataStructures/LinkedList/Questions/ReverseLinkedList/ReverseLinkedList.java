e97eab7bed347ec9ef136a63991476782cb3b4a3
Vivek Kumar <vivekkr0311@gmail.com>
2024-08-19 11:40:36 +0530

    [LinkedList]: Question Reverse the list

--- src/com/Vivek/DataStructures/LinkedList/Questions/ReverseLinkedList/ReverseLinkedList.java
+++ src/com/Vivek/DataStructures/LinkedList/Questions/ReverseLinkedList/ReverseLinkedList.java
@@ -1,10 +1,61 @@
 package com.Vivek.DataStructures.LinkedList.Questions.ReverseLinkedList;
 
 import com.Vivek.DataStructures.LinkedList.Questions.LL;
+import com.Vivek.DataStructures.LinkedList.Questions.LL.Node;
 
 import java.util.Scanner;
 
 public class ReverseLinkedList {
+
+    private static Node reverseLinkedList(Node head){
+        if(head == null){
+            return head;
+        }
+
+        Node prev = null;
+        Node curr = head;
+        Node nex = curr.next;
+        while(nex != null){
+            curr.next = prev;
+            prev = curr;
+            curr = nex;
+            nex = nex.next;
+        }
+        curr.next = prev;
+        head = curr;
+        return head;
+    }
+
+    private static Node reverseLinkedList_2(Node head){
+        if(head == null){
+            return head;
+        }
+
+        Node prev = null;
+        Node curr = head;
+        Node nex = curr.next;
+        while(curr != null){
+            curr.next = prev;
+            prev = curr;
+            curr = nex;
+            if(nex == null){
+                break;
+            }
+            nex = nex.next;
+        }
+
+        return prev;
+    }
+
+    private static void printList(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data +  " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many elements ?");
@@ -17,6 +68,11 @@ public class ReverseLinkedList {
             list.insertLast(scanner.nextInt());
         }
 
-        list.printList();
+        Node head = list.getHead();
+        printList(head);
+        head = reverseLinkedList(head);
+        printList(head);
+        head = reverseLinkedList_2(head);
+        printList(head);
     }
 }
