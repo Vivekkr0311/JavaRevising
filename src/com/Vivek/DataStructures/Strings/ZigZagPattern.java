0dc3326f0bcb374b6d495050bd32fe44fa092a27
Vivek Kumar <vivekkr0311@gmail.com>
2024-08-24 22:29:46 +0530

    [Strings]: Zigzag pattern, efficient solution

--- src/com/Vivek/DataStructures/Strings/ZigZagPattern.java
+++ src/com/Vivek/DataStructures/Strings/ZigZagPattern.java
@@ -4,25 +4,33 @@ import java.util.Scanner;
 
 public class ZigZagPattern {
 
-    private static void convert(String s, int numRows){
-        String ans = "";
-        int div = s.length() / numRows;
-        int givR = numRows;
-        numRows = numRows + 1;
-        for(int i = 0; i < s.length(); i++){
-            if(numRows > 0){
-                for(int j = i; j < s.length(); j += numRows){
-                    System.out.print(s.charAt(j) + " ");
-//                    ans = ans + s.charAt(j);
-                }
-            }else{
-                break;
-            }
-            numRows /= div;
-            System.out.println();
-        }
-//        System.out.println(ans.substring(0, ans.length() - givR));
-    }
+//    private static void convert(String s, int numRows){
+//
+//        String ans = "";
+//        int gR = numRows;
+//
+//        int k = 0;
+//        for(int rows = gR; rows > 0; rows--){
+//            if(rows == 1){
+//                for(int i = k; i < s.length(); i += 2*gR - 2){
+//                    ans = ans + s.charAt(i);
+//                }
+//                rows = 0;
+//                break;
+//            }
+//
+//            if(rows == 0){
+//                break;
+//            }
+//            for(int i = k; i < s.length(); i += 2 * rows - 2){
+//                ans = ans + s.charAt(i);
+//            }
+//            k++;
+//        }
+//
+//        System.out.println(ans);
+////        System.out.println(ans.substring(0, ans.length() - givR));
+//    }
 
     private static void convert2(String s, int numRows){
         if(numRows == 1){
@@ -42,12 +50,45 @@ public class ZigZagPattern {
         System.out.println(ans);
     }
 
+    private static void convert3(String s, int numRows){
+        if(numRows == 1){
+            System.out.println(s);
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+
+        for(int i = 0; i < numRows; i++){
+            int south = 2 * (numRows - 1 - i);
+            int north = 2 * i;
+            int idx = i;
+            boolean goingSouth = true;
+            while(idx < n){
+                sb.append(s.charAt(idx));
+                if(i == 0){
+                    idx += south;
+                }else if(i == numRows - 1){
+                    idx += north;
+                }else{
+                    if(goingSouth){
+                        idx += south;
+                    }else{
+                        idx += north;
+                    }
+                    goingSouth = !goingSouth;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the string: ");
         String s = scanner.nextLine();
         System.out.println("Enter the number of rows: ");
         int numRows = scanner.nextInt();
-        convert2(s,numRows);
+        convert3(s,numRows);
     }
 }
