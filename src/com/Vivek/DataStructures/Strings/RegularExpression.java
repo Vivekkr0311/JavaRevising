5eff888d5755d35d3ba9310eb7c11733a41b539d
Vivek Kumar <vivekkr0311@gmail.com>
2024-08-26 15:11:58 +0530

    [Strings]: Regular expression matching

---
+++ src/com/Vivek/DataStructures/Strings/RegularExpression.java
@@ -0,0 +1,79 @@
+package com.Vivek.DataStructures.Strings;
+
+import java.util.Scanner;
+
+public class RegularExpression {
+
+    private static boolean isMatch3(String s, String p){
+        if(p.length() == 0){
+            return s.length() == 0;
+        }
+
+        boolean first_character_match = (s.length() > 0) && ((p.charAt(0) == '.') || (s.charAt(0) == p.charAt(0)));
+
+        if(p.length() >1 && p.charAt(1) == '*'){
+            boolean not_take_astrisk = isMatch3(s, p.substring(2));
+            boolean take_astrisk = first_character_match && isMatch3(s.substring(1), p);
+            return not_take_astrisk || take_astrisk;
+        }
+        return first_character_match && isMatch3(s.substring(1), p.substring(1));
+    }
+
+    private static boolean isMatch2(String s, String p){
+        if(p.length() == 0){
+            if(s.length() == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        if(p.charAt(1) == '*'){
+            boolean not_take_astrisk = isMatch2(s, p.substring(2));
+            boolean take_astrisk = (p.charAt(0) == '.') || s.charAt(0) == p.charAt(0) || isMatch2(s.substring(1), p);
+            return not_take_astrisk || take_astrisk;
+        }else{
+            return (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.' || isMatch2(s.substring(1), p.substring(1)));
+        }
+    }
+
+
+    private static boolean isMatch(String s, String p){
+       if(p.length() == 0){
+           if(s.length() == 0){
+               return true;
+           }else{
+               return false;
+           }
+       }
+
+       if(p.charAt(1) == '*'){
+           boolean not_take_astrisk = isMatch(s, p.substring(2));
+           boolean take_astrisk = false;
+           if(p.charAt(0) == '.'){
+               take_astrisk = isMatch(s.substring(1), p);
+           }else{
+               if(s.charAt(0) == p.charAt(0)){
+                   take_astrisk = isMatch(s.substring(1), p);
+               }
+           }
+           return not_take_astrisk || take_astrisk;
+       }else{
+           if(s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'){
+               return isMatch(s.substring(1), p.substring(1));
+           }else{
+               return false;
+           }
+       }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter s: ");
+        String s = scanner.nextLine();
+        System.out.println("Enter p: ");
+        String p = scanner.nextLine();
+
+        System.out.println(s + " matches " + p + " : " + isMatch3(s, p));
+    }
+}
