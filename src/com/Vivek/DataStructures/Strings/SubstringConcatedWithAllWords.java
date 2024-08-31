65312c06cd97d163188b244aa43c0b1f454dd8b2
Vivek Kumar <vivekkr0311@gmail.com>
2024-08-31 17:40:52 +0530

    [Strings]: Substring concated with all words, efficient in-progress

--- src/com/Vivek/DataStructures/Strings/SubstringConcatedWithAllWords.java
+++ src/com/Vivek/DataStructures/Strings/SubstringConcatedWithAllWords.java
@@ -36,6 +36,53 @@ public class SubstringConcatedWithAllWords {
         return ans;
     }
 
+    private static HashMap<String, Integer> getMap(String[] words){
+        if(words.length == 0){
+            return new HashMap<>();
+        }
+        HashMap<String, Integer> ans = new HashMap<>();
+        for(String s : words){
+            if(!ans.containsKey(s)){
+                ans.put(s, 1);
+            }else{
+                ans.put(s, ans.get(s) + 1);
+            }
+        }
+        return ans;
+    }
+
+    private static List<Integer> findSubstring2(String s, String[] words){
+        HashMap<String, Integer> map = getMap(words);
+
+        int wordSize = words[0].length();
+        int wordArraySize = words.length;
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0;  i <= s.length() - wordSize; i += wordSize){
+            HashMap<String, Integer> currMap = new HashMap<>();
+            currMap.putAll(map);
+            for(int j = i; j < i + wordArraySize * wordSize; j += wordSize){
+                String currSubString = s.substring(j, j + wordSize);
+                if(!currMap.containsKey(currSubString) || currMap.get(currSubString) == 0){
+                    break;
+                }else{
+                    currMap.put(currSubString, currMap.get(currSubString) - 1);
+                }
+            }
+
+            boolean allZero = true;
+            for(String sub :  currMap.keySet()){
+                if(currMap.get(sub) != 0){
+                    allZero = false;
+                }
+            }
+            if(allZero){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter s: ");
@@ -50,6 +97,6 @@ public class SubstringConcatedWithAllWords {
             words[i] = scanner.nextLine();
         }
 
-        System.out.println(findSubstring(s, words));
+        System.out.println(findSubstring2(s, words));
     }
 }
