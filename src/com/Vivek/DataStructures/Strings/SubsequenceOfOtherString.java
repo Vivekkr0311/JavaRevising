dc64077f558c71a5c2a99f590a87535ef450ea36
Vivek Kumar <vivekkr0311@gmail.com>
2024-09-09 18:02:11 +0530

    [Strings]: Check if a string is subsequence of other string

---
+++ src/com/Vivek/DataStructures/Strings/SubsequenceOfOtherString.java
@@ -0,0 +1,36 @@
+package com.Vivek.DataStructures.Strings;
+
+import java.util.Scanner;
+
+public class SubsequenceOfOtherString {
+
+    private static boolean subsequenceOtherString(String mainString, String subsequence){
+        int mainStringLength = mainString.length();
+        int subsequenceLength = subsequence.length();
+        if(mainStringLength == 0 || subsequenceLength == 0){
+            return false;
+        }
+
+        int mainStringIdx = 0;
+        int subsequenceIdx = 0;
+        while(mainStringIdx < mainStringLength && subsequenceIdx < subsequenceLength){
+            if(mainString.charAt(mainStringIdx) == subsequence.charAt(subsequenceIdx)){
+                mainStringIdx++;
+                subsequenceIdx++;
+            }else{
+                mainStringIdx++;
+            }
+        }
+        return subsequenceIdx == subsequenceLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the main string: ");
+        String mainString = scanner.nextLine();
+        System.out.println("Enter the subsequence: ");
+        String subsequence = scanner.nextLine();
+
+        System.out.println(subsequence + " is subsequence of " + mainString + ": " + subsequenceOtherString(mainString, subsequence));
+    }
+}
