a1b34d24d82128b847edeb6595a8574248401825
Vivek Kumar <vivekkr0311@gmail.com>
2024-09-14 16:22:53 +0530

    [Strings]: Is Palindrome leetcode solution add

--- src/com/Vivek/DataStructures/Strings/PalindromeString.java
+++ src/com/Vivek/DataStructures/Strings/PalindromeString.java
@@ -18,6 +18,29 @@ public class PalindromeString {
         return true;
     }
 
+    private static boolean isPalindrome(String s) {
+        int forwardIdx = 0;
+        int backwardIdx = s.length() - 1;
+
+        while(forwardIdx < backwardIdx){
+
+            // skipping special characters
+            while (forwardIdx < backwardIdx && !Character.isLetterOrDigit(s.charAt(forwardIdx))) {
+                forwardIdx++;
+            }
+            while (forwardIdx < backwardIdx && !Character.isLetterOrDigit(s.charAt(backwardIdx))) {
+                backwardIdx--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(forwardIdx)) != Character.toLowerCase(s.charAt(backwardIdx))) {
+                return false;
+            }
+            forwardIdx++;
+            backwardIdx--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your string: ");
