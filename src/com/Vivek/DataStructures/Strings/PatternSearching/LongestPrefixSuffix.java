c4ddcc735e31ee006ceb04027f76e0a51cc3a931
Vivek Kumar <vivekkr0311@gmail.com>
2024-10-03 18:46:41 +0530

    [Strings]: Longest Proper Prefix Suffix

--- src/com/Vivek/DataStructures/Strings/PatternSearching/LongestPrefixSuffix.java
+++ src/com/Vivek/DataStructures/Strings/PatternSearching/LongestPrefixSuffix.java
@@ -5,43 +5,28 @@ import java.util.Scanner;
 
 public class LongestPrefixSuffix {
 
-    private static ArrayList<String> getSuffix(String s){
-        ArrayList<String> suffix = new ArrayList<>();
 
-        int n = s.length();
-        for(int i = n; i >= 0; i--){
-            suffix.add(s.substring(i, n));
-        }
-        return suffix;
-    }
-
-    private static ArrayList<String> getPrefix(String s){
-        ArrayList<String> prefix = new ArrayList<>();
-
-        for(int i = 0; i < s.length(); i++){
-            prefix.add(s.substring(0, i));
-        }
-        return prefix;
-    }
-
-    private static ArrayList<Integer> longestPrefixSuffix(String input){
-        int n = input.length();
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            String subStr = input.substring(0, i);
-            ArrayList<String> prefix = getPrefix(subStr);
-            ArrayList<String> suffix = getSuffix(subStr);
-
-            System.out.println("\nSubstring: " + subStr);
-            System.out.println("Prefix:" + prefix);
-            System.out.println("Suffix:" + suffix);
-            for(String pre : prefix){
-                for(String suff : suffix){
-                    if(pre.equalsIgnoreCase(suff)){
-                        res.add(pre.length());
-                    }
+    private static long longestPrefixSuffix(String input, int n){
+        for(int len = n - 1; len > 0; len--){
+            boolean flag = true;
+            for(int i = 0; i < len; i++){
+                if(input.charAt(i) != input.charAt(n - len + i)){
+                    flag = false;
                 }
             }
+
+            if(flag){
+                return len;
+            }
+        }
+        return 0;
+    }
+
+    private static ArrayList<Long> fillLongestProperPrefixSuffix(String input){
+        ArrayList<Long> res = new ArrayList<>();
+        int n = input.length();
+        for(int i = 0; i < n; i++){
+            res.add(longestPrefixSuffix(input, i + 1));
         }
         return res;
     }
@@ -51,6 +36,6 @@ public class LongestPrefixSuffix {
         System.out.println("Enter the string: ");
         String input = scanner.nextLine();
 
-        System.out.println("Longest prefix suffix for each idx of " + input + " are " + longestPrefixSuffix(input));
+        System.out.println("Longest prefix suffix for each idx of " + input + " are " + fillLongestProperPrefixSuffix(input));
     }
 }
