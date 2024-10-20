9d138bbbb77e90fc36fa11e06ef3204a26b87a78
Vivek Kumar <vivekkr0311@gmail.com>
2024-10-20 14:05:20 +0530

    [Binary Search]: Question, Intersection Of Arrays II

--- src/com/Vivek/SearchingQuestions/BinarySearch/IntersectionOfTwoArrays.java
+++ src/com/Vivek/SearchingQuestions/BinarySearch/IntersectionOfTwoArrays.java
@@ -1,9 +1,6 @@
 package com.Vivek.SearchingQuestions.BinarySearch;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class IntersectionOfTwoArrays {
 
@@ -44,6 +41,43 @@ public class IntersectionOfTwoArrays {
         return ans;
     }
 
+    private static int[] intersection2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int i = 0, j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] != nums2[j]){
+                if(nums1[i] < nums2[j]){
+                    i++;
+                }else{
+                    j++;
+                }
+            } else if(nums1[i] == nums2[j]){
+                list.add(nums1[i]);
+
+                while(i + 1 < nums1.length && nums1[i] == nums1[i + 1]){
+                    i++;
+                }
+
+                while(j + 1 < nums2.length && nums2[j] == nums2[j + 1]){
+                    j++;
+                }
+                i++;
+                j++;
+            }
+        }
+
+
+        int[] ans = new int[list.size()];
+        int idx = 0;
+        for(int num : list){
+            ans[idx++] = num;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many elements? arr1: ");
@@ -64,6 +98,7 @@ public class IntersectionOfTwoArrays {
             arr2[i] = scanner.nextInt();
         }
 
-        System.out.println(Arrays.toString(intersection(arr1, arr2)));
+//        System.out.println(Arrays.toString(intersection(arr1, arr2)));
+        System.out.println(Arrays.toString(intersection2(arr1, arr2)));
     }
 }
---
+++ src/com/Vivek/SearchingQuestions/BinarySearch/IntersectionOfTwoArraysII.java
@@ -0,0 +1,57 @@
+package com.Vivek.SearchingQuestions.BinarySearch;
+
+import java.util.*;
+
+public class IntersectionOfTwoArraysII {
+
+    private static int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i = 0, j = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] != nums2[j]){
+                if(nums1[i] < nums2[j]){
+                    i++;
+                }else{
+                    j++;
+                }
+            }else{
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        int[] ans = new int[list.size()];
+        int idx = 0;
+        for(int num : list){
+            ans[idx++] = num;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many elements? arr1: ");
+        int n1 = scanner.nextInt();
+
+        int[] arr1 = new int[n1];
+        System.out.print("Insert each elements: arr1 ");
+        for(int i = 0; i < n1; i++){
+            arr1[i] = scanner.nextInt();
+        }
+
+        System.out.println("How many elements? arr12 ");
+        int n2 = scanner.nextInt();
+
+        int[] arr2 = new int[n2];
+        System.out.print("Insert each elements: arr2 ");
+        for(int i = 0; i < n2; i++){
+            arr2[i] = scanner.nextInt();
+        }
+
+        System.out.println(Arrays.toString(intersection(arr1, arr2)));
+    }
+}
