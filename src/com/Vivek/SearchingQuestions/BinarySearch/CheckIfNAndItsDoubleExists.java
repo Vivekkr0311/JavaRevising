67292a514b89b07e6891d5053cfef9e92288466c
Vivek Kumar <vivekkr0311@gmail.com>
2024-11-01 12:04:43 +0530

    [Binary Search]: Question, Check If N And Its Double Exists, using Binary Search

--- src/com/Vivek/SearchingQuestions/BinarySearch/CheckIfNAndItsDoubleExists.java
+++ src/com/Vivek/SearchingQuestions/BinarySearch/CheckIfNAndItsDoubleExists.java
@@ -1,5 +1,6 @@
 package com.Vivek.SearchingQuestions.BinarySearch;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CheckIfNAndItsDoubleExists {
@@ -10,6 +11,36 @@ public class CheckIfNAndItsDoubleExists {
                 if(i != j && arr[i] == arr[j] * 2){
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    private static int search(int[] arr, int target){
+        int startIdx = 0;
+        int endIdx = arr.length - 1;
+
+        while(startIdx <= endIdx){
+            int mid = startIdx + ((endIdx - startIdx) >> 1);
+
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] > target){
+                endIdx = mid - 1;
+            }else{
+                startIdx = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean checkIfNAndItsDoubleExists(int[] arr){
+        Arrays.sort(arr);
+
+        for(int i = 0; i < arr.length; i++){
+            int foundIdx = search(arr, arr[i] * 2);
+            if(foundIdx != -1 && foundIdx != i){
+                return true;
             }
         }
         return false;
@@ -27,5 +58,6 @@ public class CheckIfNAndItsDoubleExists {
         }
 
         System.out.println("If N and its double exists: " + checkIfNAndItsDoubleExistsBruteForce(arr));
+        System.out.println("If N and its double exists: " + checkIfNAndItsDoubleExists(arr));
     }
 }
