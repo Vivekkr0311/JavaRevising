7954bfac96cd8600651fa38e45859d673786ff12
Vivek Kumar <vivekkr0311@gmail.com>
2024-11-20 17:33:29 +0530

    [Binary Search]: Count Pairs Whose Sum Is Less Than Target, Binary Search

--- src/com/Vivek/SearchingQuestions/BinarySearch/CountPairsWhoseSumIsLessThanTarget.java
+++ src/com/Vivek/SearchingQuestions/BinarySearch/CountPairsWhoseSumIsLessThanTarget.java
@@ -1,6 +1,7 @@
 package com.Vivek.SearchingQuestions.BinarySearch;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,6 +23,33 @@ public class CountPairsWhoseSumIsLessThanTarget {
         return count;
     }
 
+    private static int countPairs(List<Integer> nums, int target){
+        Collections.sort(nums);
+
+        int n = nums.size() - 1;
+        int count = 0;
+        for(int i = 0; i <= n; i++){
+            int remaining = target - nums.get(i);
+
+            int j = search(nums, remaining, i + 1, n);
+            count = count + (j - i);
+        }
+        return count;
+    }
+
+    private static int search(List<Integer> nums, int target, int start, int end){
+        while(start <= end){
+            int mid = start + ((end - start) >> 1);
+
+            if(nums.get(mid) < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many element do you have? ");
@@ -37,5 +65,6 @@ public class CountPairsWhoseSumIsLessThanTarget {
         int target = scanner.nextInt();
 
         System.out.println("Total pairs possible: " + countPairsBruteForce(nums, target));
+        System.out.println("Total pairs possible: " + countPairs(nums, target));
     }
 }
