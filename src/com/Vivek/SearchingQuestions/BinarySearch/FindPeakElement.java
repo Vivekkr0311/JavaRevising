b2d1321449251128b34712600d549124e3d38477
Vivek Kumar <vivekkr0311@gmail.com>
2024-12-04 19:56:29 +0530

    [Binary Search]: Find Peak Element, Binary Search with less checks, this could find different peak but the answer will be correct

--- src/com/Vivek/SearchingQuestions/BinarySearch/FindPeakElement.java
+++ src/com/Vivek/SearchingQuestions/BinarySearch/FindPeakElement.java
@@ -4,6 +4,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindPeakElement {
+    private static int findPeakElementTwo(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start < end){
+            int mid = start + ((end - start) >> 1);
+
+            if(nums[mid] > nums[mid + 1]){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+
     private static int findPeakElement(int[] nums){
         int start = 0;
         int end = nums.length - 1;
@@ -57,6 +73,8 @@ public class FindPeakElement {
         }
 
         int peak = findPeakElement(nums);
+        int peak2 = findPeakElementTwo(nums);
         System.out.println("Peak of " + Arrays.toString(nums) + " is at " + peak +  " which is " + nums[peak]);
+        System.out.println("Peak of " + Arrays.toString(nums) + " is at " + peak2 +  " which is " + nums[peak2]);
     }
 }
