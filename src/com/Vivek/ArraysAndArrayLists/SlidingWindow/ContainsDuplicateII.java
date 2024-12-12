a09bd3b446d7b12a7be5884a02ef6cbf4f7aff40
Vivek Kumar <vivekkr0311@gmail.com>
2024-12-12 18:30:55 +0530

    [Sliding Window]: Contains Duplicate II, Sliding Window using while loop

--- src/com/Vivek/ArraysAndArrayLists/SlidingWindow/ContainsDuplicateII.java
+++ src/com/Vivek/ArraysAndArrayLists/SlidingWindow/ContainsDuplicateII.java
@@ -20,6 +20,28 @@ public class ContainsDuplicateII {
        return false;
     }
 
+    private static boolean containsNearbyDuplicateTwo(int[] nums, int k){
+        Set<Integer> window = new HashSet<>();
+        int n = nums.length;
+        int i = 0; int j = 0;
+
+        while(j < n){
+            if(Math.abs(i - j) > k){
+                // shrink
+                i++;
+                window.remove(nums[i]);
+            }
+
+            if(window.contains(nums[j])){
+                return true;
+            }
+
+            window.add(nums[j]);
+            j++;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many numbers ? ");
@@ -33,5 +55,6 @@ public class ContainsDuplicateII {
         System.out.println("Enter k: ");
         int k = scanner.nextInt();
         System.out.println(Arrays.toString(nums) + " contains duplicate " + containsNearbyDuplicate(nums, k));
+        System.out.println(Arrays.toString(nums) + " contains duplicate " + containsNearbyDuplicateTwo(nums, k));
     }
 }
