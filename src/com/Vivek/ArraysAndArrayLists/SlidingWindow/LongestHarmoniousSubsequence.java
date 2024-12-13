df35701e65ee02de355de13a77878109ea1b651f
Vivek Kumar <vivekkr0311@gmail.com>
2024-12-13 17:22:49 +0530

    [Sliding Window]: Longest Harmonious Subsequence, Sliding Window approach

--- src/com/Vivek/ArraysAndArrayLists/SlidingWindow/LongestHarmoniousSubsequence.java
+++ src/com/Vivek/ArraysAndArrayLists/SlidingWindow/LongestHarmoniousSubsequence.java
@@ -1,11 +1,12 @@
 package com.Vivek.ArraysAndArrayLists.SlidingWindow;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class LongestHarmoniousSubsequence {
 
-    private static int findLHS(int[] nums){
+    private static int findLHSHashMapApproach(int[] nums){
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int n : nums){
@@ -23,6 +24,24 @@ public class LongestHarmoniousSubsequence {
         return ans;
     }
 
+    // Actual sliding window approach
+    private static int findLHSTwo(int[] nums){
+        Arrays.sort(nums);
+        int forward = 0;
+        int ans = 0;
+        for(int backward = 0; backward < nums.length; backward++){
+
+            if(nums[backward] - nums[forward] > 1){
+                forward++;
+            }
+
+            if(nums[backward] - nums[forward] == 1){
+                ans = Math.max(ans, backward - forward + 1);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many element ? ");
@@ -33,6 +52,7 @@ public class LongestHarmoniousSubsequence {
             nums[i] = scanner.nextInt();
         }
 
-        System.out.println("Longest harmonious subsequence length is: " + findLHS(nums));
+        System.out.println("Longest harmonious subsequence length is: " + findLHSHashMapApproach(nums));
+        System.out.println("Longest harmonious subsequence length is: " + findLHSTwo(nums));
     }
 }
