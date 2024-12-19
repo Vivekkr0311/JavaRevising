09e1b9a3d89eb4f726ef40820b78c2d29b0ab4f0
Vivek Kumar <vivekkr0311@gmail.com>
2024-12-19 16:05:33 +0530

    [Sliding Window]: Substrings of size three with distinct characters, Sliding Window

--- src/com/Vivek/ArraysAndArrayLists/SlidingWindow/SubstringsOfSizeThreeWithDistinctCharacters.java
+++ src/com/Vivek/ArraysAndArrayLists/SlidingWindow/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -1,12 +1,11 @@
 package com.Vivek.ArraysAndArrayLists.SlidingWindow;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class SubstringsOfSizeThreeWithDistinctCharacters {
-    private static int countGoodSubStrings(String s){
+    private static int countGoodSubStringsBruteForce(String s){
         int count = 0;
         for(int i = 0; i + 3 <= s.length(); i++){
             Set<Character> set = new HashSet<>();
@@ -28,11 +27,30 @@ public class SubstringsOfSizeThreeWithDistinctCharacters {
         return count;
     }
 
+    private static int countGoodSubStrings(String s){
+        if(s.length() < 3){
+            return 0;
+        }
+
+        int count = 0;
+        for(int i = 0; i <= s.length() - 3; i++){
+            char f = s.charAt(i);
+            char sec = s.charAt(i + 1);
+            char t = s.charAt(i + 2);
+
+            if(f != sec && sec != t && f != t){
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the string: ");
         String s = scanner.nextLine();
 
+        System.out.println("Number of good substring in " + s + " is " + countGoodSubStringsBruteForce(s));
         System.out.println("Number of good substring in " + s + " is " + countGoodSubStrings(s));
     }
 }
