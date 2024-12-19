4038aed226f7228a9fb7d98d22116a38b835402d
Vivek Kumar <vivekkr0311@gmail.com>
2024-12-19 14:36:10 +0530

    [Sliding Window]: Longest Nice Substring, Sliding window but its slower than Divied And Conquer approach

--- src/com/Vivek/ArraysAndArrayLists/SlidingWindow/LongestNiceSubstring.java
+++ src/com/Vivek/ArraysAndArrayLists/SlidingWindow/LongestNiceSubstring.java
@@ -1,5 +1,6 @@
 package com.Vivek.ArraysAndArrayLists.SlidingWindow;
 
+import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -29,7 +30,7 @@ public class LongestNiceSubstring {
         return true;
     }
 
-    private static String longestNiceSubstring(String s){
+    private static String longestNiceSubstringBruteForce(String s){
         int n = s.length();
         String ans = "";
         for(int i = 0; i < n; i++){
@@ -47,11 +48,56 @@ public class LongestNiceSubstring {
         return ans;
     }
 
+    private static boolean isNice(Set<Character> lowerCase, Set<Character> upperCase){
+        for(char ch : lowerCase){
+            char upperCaseVersion = Character.toUpperCase(ch);
+            if(!(upperCase.contains(upperCaseVersion))){
+                return false;
+            }
+        }
+
+        for(char ch : upperCase){
+            char lowerCaseVersion = Character.toLowerCase(ch);
+
+            if(!(lowerCase.contains(lowerCaseVersion))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String longestNiceSubstring(String s){
+        int n = s.length();
+        String ans = "";
+        for(int start = 0; start < n; start++){
+            Set<Character> lowerCase = new HashSet<>();
+            Set<Character> upperCase = new HashSet<>();
+
+            for(int end = start; end < n; end++){
+                char ch = s.charAt(end);
+                if(Character.isLowerCase(ch)){
+                    lowerCase.add(ch);
+                }else if(Character.isUpperCase(ch)){
+                    upperCase.add(ch);
+                }
+
+                if(isNice(lowerCase, upperCase)){
+                    if(end - start + 1 > ans.length()){
+                        ans = s.substring(start, end + 1);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your string: ");
         String s = scanner.nextLine();
 
+        System.out.println("Longest nice substring is: " + longestNiceSubstringBruteForce(s));
         System.out.println("Longest nice substring is: " + longestNiceSubstring(s));
     }
 }
