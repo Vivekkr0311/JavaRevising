bdf07d64f64956ab535ba50f4272f9008f1b2fa8
Vivek Kumar <vivekkr0311@gmail.com>
2025-01-01 16:26:23 +0530

    [Sliding Window]: Cound subtrings that satisfy K constraint I, Sliding Window approach

--- src/com/Vivek/ArraysAndArrayLists/SlidingWindow/CountSubstringsThatSatisfyKContraintI.java
+++ src/com/Vivek/ArraysAndArrayLists/SlidingWindow/CountSubstringsThatSatisfyKContraintI.java
@@ -16,15 +16,46 @@ public class CountSubstringsThatSatisfyKContraintI {
         return zeroCount <= k || oneCount <= k;
     }
 
-    private static int countKConstraintSubstrings(String s, int k){
+    private static int countKConstraintSubstringsBruteForce(String s, int k){
         int ans = 0;
         int n = s.length();
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
                 if (doesContainAtMostK(s.substring(i, j + 1), k)){
                     ans++;
+                }else{
+                    break;
                 }
             }
+        }
+        return ans;
+    }
+
+    private static int countKConstraintSubstrings(String s, int k){
+        int ans = 0;
+        int left = 0;
+        int right = left;
+        int countOnes = 0;
+        int countZeros = 0;
+        int n = s.length();
+
+        while(right < n){
+            if(s.charAt(right) == '0'){
+                countZeros++;
+            }else if(s.charAt(right) == '1'){
+                countOnes++;
+            }
+
+            while(countOnes > k && countZeros > k){
+                if(s.charAt(left) == '0'){
+                    countZeros--;
+                }else if(s.charAt(left) == '1'){
+                    countOnes--;
+                }
+                left++;
+            }
+            ans += right - left + 1;
+            right++;
         }
         return ans;
     }
@@ -37,6 +68,7 @@ public class CountSubstringsThatSatisfyKContraintI {
         int k = scanner.nextInt();
 
 
-        System.out.println("Number of substring that contains at most " + k + " 0s and 1s: " + countKConstraintSubstrings(binary, k));
+        System.out.println("Number of substring that contains at most " + k + ", 0s and 1s: " + countKConstraintSubstringsBruteForce(binary, k));
+        System.out.println("Number of substring that contains at most " + k + ", 0s and 1s: " + countKConstraintSubstrings(binary, k));
     }
 }
