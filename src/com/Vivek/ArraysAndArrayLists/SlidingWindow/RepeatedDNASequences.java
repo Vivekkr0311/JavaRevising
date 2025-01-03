04cad4a73f47d119f2eacb68663cb8cffd91ca20
Vivek Kumar <vivekkr0311@gmail.com>
2025-01-03 11:43:33 +0530

    [Sliding Window]: Repeated DNA sequences, Sliding Window but faster than previous approach

--- src/com/Vivek/ArraysAndArrayLists/SlidingWindow/RepeatedDNASequences.java
+++ src/com/Vivek/ArraysAndArrayLists/SlidingWindow/RepeatedDNASequences.java
@@ -1,12 +1,9 @@
 package com.Vivek.ArraysAndArrayLists.SlidingWindow;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class RepeatedDNASequences {
-    private static List<String> findRepeatedDnaSequences(String s){
+    private static List<String> findRepeatedDnaSequencesSlidingWindowNaive(String s){
         List<String> ans = new ArrayList<>();
 
         HashMap<String, Integer> map = new HashMap<>();
@@ -32,11 +29,44 @@ public class RepeatedDNASequences {
         return ans;
     }
 
+    private static List<String> findRepeatedDnaSequences2(String s){
+        Set<String> storage1 = new HashSet<>(), storage2 = new HashSet<>();
+        int left = 0;
+        int right = left + 10;
+        int n = s.length();
+        while(right <= n){
+            String subString = s.substring(left, right);
+
+            if(!storage1.add(subString)){
+                storage2.add(subString);
+            }
+            left++;
+            right++;
+        }
+
+        return new ArrayList<>(storage2);
+    }
+
+    private static List<String> findRepeatedDnaSequences(String s){
+        Set<String> storage1 = new HashSet<>(), storage2 = new HashSet<>();
+        int n = s.length();
+        for(int i = 0; i < n - 9; i++){
+            String subsString = s.substring(i, i + 10);
+            if(!storage1.add(subsString)){
+                storage2.add(subsString);
+            }
+        }
+
+        return new ArrayList<>(storage2);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the DNA sequence: ");
         String s = scanner.nextLine();
 
-        System.out.println("10-letter DNA sequence which occurred more than once are:" + findRepeatedDnaSequences(s));
+        System.out.println("10-letter DNA sequence which occurred more than once are: " + findRepeatedDnaSequencesSlidingWindowNaive(s));
+        System.out.println("10-letter DNA sequence which occurred more than once are: " + findRepeatedDnaSequences2(s));
+        System.out.println("10-letter DNA sequence which occurred more than once are: " + findRepeatedDnaSequences(s));
     }
 }
