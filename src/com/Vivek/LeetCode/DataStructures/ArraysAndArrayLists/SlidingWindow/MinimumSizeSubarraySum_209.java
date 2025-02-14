1cc2c7bfa3bb75b088552bfea051cf0d97716a36
Vivek Kumar <vivekkr0311@gmail.com>
2025-02-14 10:40:37 +0530

    [Sliding Window]: 209. Minimum size subarray sum

--- src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/MinimumSizeSubarraySum_209.java
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/MinimumSizeSubarraySum_209.java
@@ -1,8 +1,8 @@
-package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.SearchingQuestions.BinarySearch;
+package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.SlidingWindow;
 
 import java.util.Scanner;
 
-public class MinimumSizeSubarraySum {
+public class MinimumSizeSubarraySum_209 {
     private static int search(int[] nums, int start, int end, int target) {
         int ans = -1;
         while (start <= end) {
@@ -19,6 +19,7 @@ public class MinimumSizeSubarraySum {
     }
 
     private static int minSubArrayLen(int target, int[] nums) {
+        // Binary search approach, but I think this question is most suited for sliding window
         int[] prefix = new int[nums.length];
         prefix[0] = nums[0];
 
@@ -39,6 +40,44 @@ public class MinimumSizeSubarraySum {
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 
+    private static int minSubArrayLenBruteForce(int target, int[] nums) {
+        int ansLength = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int local_sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                local_sum += nums[j];
+
+                if (local_sum >= target) {
+                    if (ansLength > j - i + 1) {
+                        ansLength = j - i + 1;
+                    }
+                }
+            }
+        }
+
+        return ansLength == Integer.MAX_VALUE ? 0 : ansLength;
+    }
+
+    private static int minSubArrayLenSlidingWindow(int target, int[] nums) {
+        int ansLength = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        int n = nums.length;
+        int currSum = 0;
+
+        while (j < n) {
+            currSum += nums[j];
+
+            while (currSum >= target) {
+                ansLength = Math.min(ansLength, j - i + 1);
+                currSum -= nums[i];
+                i++;
+            }
+            j++;
+        }
+        return ansLength == Integer.MAX_VALUE ? 0 : ansLength;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many elements? ");
@@ -46,7 +85,7 @@ public class MinimumSizeSubarraySum {
 
         int[] nums = new int[n];
         System.out.println("Enter each element: ");
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             nums[i] = scanner.nextInt();
         }
 
@@ -54,5 +93,7 @@ public class MinimumSizeSubarraySum {
         int target = scanner.nextInt();
 
         System.out.println("Minimum subarray size is: " + minSubArrayLen(target, nums));
+        System.out.println("Minimum subarray size is: " + minSubArrayLenBruteForce(target, nums));
+        System.out.println("Minimum subarray size is: " + minSubArrayLenSlidingWindow(target, nums));
     }
 }
