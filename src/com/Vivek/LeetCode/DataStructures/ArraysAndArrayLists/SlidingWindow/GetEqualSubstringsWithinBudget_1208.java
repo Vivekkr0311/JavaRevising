6cbac8484047915fb8930b59521a85592804e506
Vivek Kumar <vivekkr0311@gmail.com>
2025-02-17 12:50:29 +0530

    [Sliding Window]: 1208. Get equal substrings within budget

---
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/GetEqualSubstringsWithinBudget_1208.java
@@ -0,0 +1,44 @@
+package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.SlidingWindow;
+
+import java.util.Scanner;
+
+public class GetEqualSubstringsWithinBudget_1208 {
+    private static int equalSubstringBruteForce(String s, String t, int maxCost) {
+        int ans = Integer.MIN_VALUE;
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        int currCost = 0;
+        while (i < n && j < n) {
+            if (s.charAt(j) != t.charAt(j)) {
+                currCost += Math.abs(s.charAt(j) - t.charAt(j));
+            }
+
+            if (currCost <= maxCost) {
+                ans = Math.max(ans, j - i + 1);
+            }
+
+            while (currCost > maxCost) {
+                currCost -= Math.abs(s.charAt(i) - t.charAt(i));
+                i++;
+            }
+            j++;
+        }
+
+        return ans == Integer.MIN_VALUE ? 0 : ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter s: ");
+        String s = scanner.nextLine();
+
+        System.out.println("Enter t: ");
+        String t = scanner.nextLine();
+
+        System.out.println("Enter maxCost: ");
+        int maxCost = scanner.nextInt();
+
+        System.out.println("Cost to make " + s + " to " + t + " is " + equalSubstringBruteForce(s, t, maxCost));
+    }
+}
