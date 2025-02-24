545e9e5d6321fd37f57f4bd2869a0751f83a38d9
Vivek Kumar <vivekkr0311@gmail.com>
2025-02-24 12:48:27 +0530

    [Sliding Window]: 1234. Replace the substring for balanced string

---
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/ReplaceTheSubstringForBalancedString_1234.java
@@ -0,0 +1,44 @@
+package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.SlidingWindow;
+
+import java.util.Scanner;
+
+public class ReplaceTheSubstringForBalancedString_1234 {
+    private static int balancedString(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        int target = n / 4;
+        int ansLength = n;
+        int[] chars = new int[26];
+
+        for (int k = 0; k < n; k++) {
+            chars[s.charAt(k) - 'A']++;
+        }
+
+        while (j < n) {
+            chars[s.charAt(j) - 'A']--;
+
+            while (i < n &&
+                    chars['Q' - 'A'] <= target &&
+                    chars['W' - 'A'] <= target &&
+                    chars['R' - 'A'] <= target &&
+                    chars['E' - 'A'] <= target) {
+
+                chars[s.charAt(i) - 'A']++;
+
+                ansLength = Math.min(ansLength, j - i + 1);
+                i++;
+            }
+            j++;
+        }
+        return ansLength == n ? 0 : ansLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String s = scanner.nextLine();
+
+        System.out.println("Length of substring to replace: " + balancedString(s));
+    }
+}
