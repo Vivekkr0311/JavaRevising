3ccafe1a4e00bee4ef0f74db03778d609897fef0
Vivek Kumar <vivekkr0311@gmail.com>
2025-02-25 18:47:28 +0530

    [Sliding Window]: 1248. Count number of nice subarrays

--- src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/CountNumberOfNiceSubarrays_1248.java
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/CountNumberOfNiceSubarrays_1248.java
@@ -1,6 +1,50 @@
 package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.SlidingWindow;
 
-public class CountNumberOfNiceSubarray_1248 {
+import java.util.Scanner;
 
-    
+public class CountNumberOfNiceSubarrays_1248 {
+    private static int numberOfSubarrays(int[] nums, int k) {
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        int numberOfOdd = 0;
+        int count = 0;
+        int n = nums.length;
+
+        while (j < n) {
+            if ((nums[j] & 1) != 0) {
+                numberOfOdd++;
+                count = 0;
+            }
+
+            while (numberOfOdd == k) {
+                if ((nums[i] & 1) != 0) {
+                    numberOfOdd--;
+                }
+                count++;
+                i++;
+            }
+
+            ans += count;
+            j++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter each element: ");
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter k: ");
+        int k = scanner.nextInt();
+
+        System.out.println("Number of nice subarrays is: " + numberOfSubarrays(nums, k));
+    }
 }
