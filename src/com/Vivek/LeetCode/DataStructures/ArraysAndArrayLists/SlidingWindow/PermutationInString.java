9ea7b7a0e2341550ff9acd0893f36397e8cfa748
Vivek Kumar <vivekkr0311@gmail.com>
2025-02-26 13:27:35 +0530

    [Sliding Window]: 567. Permutation in String, Brute force appraoch

--- src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/PermutationInString.java
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/PermutationInString.java
@@ -1,5 +1,6 @@
 package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.SlidingWindow;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PermutationInString {
@@ -40,6 +41,46 @@ public class PermutationInString {
         return false;
     }
 
+    private static boolean checkAnagram(int[] map, char[] charArrayS2, int start, int end) {
+        int[] tempMap = new int[26];
+        System.arraycopy(map, 0, tempMap, 0, 26); // Copy map to tempMap
+
+        for (int i = start; i < end; i++) {
+            tempMap[charArrayS2[i] - 'a']--;
+        }
+
+        for (int count : tempMap) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean checkInclusionBruteForce(String s1, String s2) {
+        int n = s2.length();
+        int m = s1.length();
+        if (m > n) return false;
+
+        int[] map = new int[26];
+        char[] charArrayS2 = s2.toCharArray();
+
+        // Count frequency of characters in s1
+        for (char c : s1.toCharArray()) {
+            map[c - 'a']++;
+        }
+
+        // Check every substring of length m in s2
+        for (int i = 0; i <= n - m; i++) {
+            if (checkAnagram(map, charArrayS2, i, i + m)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter s1: ");
@@ -48,6 +89,7 @@ public class PermutationInString {
         System.out.println("Enter s2: ");
         String s2 = scanner.nextLine();
 
-        System.out.println(s2 + " contains permutation of " + s2 + ": " + checkInclusion(s1, s2));
+        System.out.println(s2 + " contains permutation of " + s1 + ": " + checkInclusion(s1, s2));
+        System.out.println(s2 + " contains permutation of " + s1 + ": " + checkInclusionBruteForce(s1, s2));
     }
 }
