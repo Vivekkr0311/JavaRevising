1bc2467828b28fe6ea2d4b03e9646a8b664f74bd
Vivek Kumar <vivekkr0311@gmail.com>
2025-03-01 16:39:51 +0530

    [Sliding Window]: 1297. Maximum Number of Occurencess of a Substring,Sliding Window approach

--- src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/MaximumNumberOfOccurrencesOfASubstring_1297.java
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/SlidingWindow/MaximumNumberOfOccurrencesOfASubstring_1297.java
@@ -35,14 +35,50 @@ public class MaximumNumberOfOccurrencesOfASubstring_1297 {
         }
 
         for (Integer val : freqMap.values()) {
-            ans =  Math.max(ans, val);
+            ans = Math.max(ans, val);
         }
         return ans;
     }
 
-    private static int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+    private static int countUniqueCharacters(int[] freqMap) {
+        int numberOfUniqueCharacters = 0;
+        for (int i : freqMap) {
+            if (i > 0) {
+                numberOfUniqueCharacters += 1;
+            }
+        }
+        return numberOfUniqueCharacters;
+    }
 
-        return 0;
+    private static int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+        int left = 0;
+        int right = 0;
+        int n = s.length();
+        char[] charString = s.toCharArray();
+        int[] freqMap = new int[26];
+        HashMap<String, Integer> stringMap = new HashMap<>();
+
+        while (right < n) {
+            freqMap[charString[right] - 'a']++;
+
+            if (right - left + 1 == minSize) {
+                if (countUniqueCharacters(freqMap) <= maxLetters) {
+                    String subString = s.substring(left, right + 1);
+                    stringMap.put(subString, stringMap.getOrDefault(subString, 0) + 1);
+                }
+
+                freqMap[charString[left] - 'a']--;
+                left++;
+            }
+
+            right++;
+        }
+
+        int ans = 0;
+        for (Integer val : stringMap.values()) {
+            ans = Math.max(ans, val);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
