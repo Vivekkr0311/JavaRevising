9a78bdbbabc604bc7fe24f936632803235d66b0b
Vivek Kumar <vivekkr0311@gmail.com>
2025-04-17 16:59:21 +0530

    [Arrays]: 2176. Count Equal and Divisible pairs in an array

--- src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/TwoPointer/CountEqualAndDivisibleParisInAnArray_2176.java
+++ src/com/Vivek/LeetCode/DataStructures/ArraysAndArrayLists/TwoPointer/CountEqualAndDivisibleParisInAnArray_2176.java
@@ -1,8 +1,56 @@
 package com.Vivek.LeetCode.DataStructures.ArraysAndArrayLists.TwoPointer;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CountEqualAndDivisibleParisInAnArray_2176 {
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private static int countPairs_(int[] nums, int k) {
+        int result = 0;
+        int n = nums.length;
+
+        Map<Integer, List<Integer>> indicesMap = new HashMap<>();
+        // storing indices of each number
+        for (int i = 0; i < n; i++) {
+            indicesMap.computeIfAbsent(nums[i], x -> new ArrayList<>()).add(i);
+        }
+
+        Set<Integer> div = new HashSet<>();
+        // computing all factors of k
+        for (int f = 1; f * f <= k; f++) {
+            if (k % f == 0) {
+                div.add(f);
+                div.add(k / f);
+            }
+        }
+
+        for (Map.Entry<Integer, List<Integer>> entry : indicesMap.entrySet()) {
+            List<Integer> indices = entry.getValue();
+
+            Map<Integer, Integer> factorsMap = new HashMap<>();
+
+            for (int i : indices) {
+                int gcd = gcd(i, k);
+                int j = k / gcd;
+
+                result += factorsMap.getOrDefault(j, 0);
+                for (int f : div) {
+                    if (i % f == 0) {
+                        factorsMap.put(f, factorsMap.getOrDefault(f, 0) + 1);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     private static int countPairs(int[] nums, int k) {
         int result = 0;
         int n = nums.length;
@@ -32,5 +80,6 @@ public class CountEqualAndDivisibleParisInAnArray_2176 {
         int k = scanner.nextInt();
 
         System.out.println("Number of good pairs: " + countPairs(nums, k));
+        System.out.println("Number of good pairs: " + countPairs_(nums, k));
     }
 }
