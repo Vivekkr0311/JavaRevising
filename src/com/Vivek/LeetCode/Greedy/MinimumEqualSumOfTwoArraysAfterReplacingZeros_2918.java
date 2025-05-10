6e95c089bc9e05d9864d1ca83aae6d604f22c901
Vivek Kumar <vivekkr0311@gmail.com>
2025-05-10 11:06:44 +0530

    [Greedy][Streams]: 2918. Minimum equal sum of two arrays after replacing zeros

--- src/com/Vivek/LeetCode/Greedy/MinimumEqualSumOfTwoArraysAfterReplacingZeros_2918.java
+++ src/com/Vivek/LeetCode/Greedy/MinimumEqualSumOfTwoArraysAfterReplacingZeros_2918.java
@@ -1,13 +1,46 @@
 package com.Vivek.LeetCode.Greedy;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MinimumEqualSumOfTwoArraysAfterReplacingZeros_2918 {
-    private static int minSum(int[] nums1, int[] nums2) {
+    private static long minSumUsingStreams(int[] nums1, int[] nums2) {
+        AtomicInteger cntZeros1 = new AtomicInteger(0);
+        AtomicInteger cntZeros2 = new AtomicInteger(0);
+
+        long sum1 = Arrays.stream(nums1)
+                .map(x -> {
+                    if(x == 0) cntZeros1.incrementAndGet();
+                    return x == 0 ? 1 : x;
+                })
+                .asLongStream()
+                .sum();
+
+        long sum2 = Arrays.stream(nums2)
+                .map(x -> {
+                    if(x == 0) cntZeros2.incrementAndGet();
+                    return x == 0 ? 1 : x;
+                }).asLongStream()
+                .sum();
+
+        if(sum1 < sum2 && cntZeros1.get() == 0){
+            return -1;
+        }
+
+        if(sum1 > sum2 && cntZeros2.get() == 0){
+            return -1;
+        }
+
+        return Math.max(sum1, sum2);
+    }
+
+    private static long minSum(int[] nums1, int[] nums2) {
         long sum1 = 0;
         long sum2 = 0;
         int countNum1Zero = 0;
         int countNum2Zero = 0;
+
         for (int i = 0; i < nums1.length; i++) {
             if (nums1[i] == 0) {
                 sum1 += 1;
@@ -58,5 +91,6 @@ public class MinimumEqualSumOfTwoArraysAfterReplacingZeros_2918 {
         }
 
         System.out.println("Answer: " + minSum(nums1, nums2));
+        System.out.println("Answer: " + minSumUsingStreams(nums1, nums2));
     }
 }
