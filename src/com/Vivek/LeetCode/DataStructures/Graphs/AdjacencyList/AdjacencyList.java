da0bacd7c206174eca2b223d26db23579d39edf5
Vivek Kumar <vivekkr0311@gmail.com>
2025-05-31 19:32:40 +0530

    [Graph][Adjacency List]: Graph adjacency list

---
+++ src/com/Vivek/LeetCode/DataStructures/Graphs/AdjacencyList/AdjacencyList.java
@@ -0,0 +1,36 @@
+package com.Vivek.LeetCode.DataStructures.Graphs.AdjacencyList;
+
+import java.util.*;
+
+public class AdjacencyList {
+    private static Map<Integer, List<Integer>> createGraph(int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+
+            // for undirected graph, in case of directed just remove the below line
+            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+        }
+        return graph;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many edges: ");
+        int e = scanner.nextInt();
+        int[][] edges = new int[e][2];
+
+        System.out.println("Enter each edge: ");
+        for (int i = 0; i < e; i++) {
+            edges[i][0] = scanner.nextInt();
+            edges[i][1] = scanner.nextInt();
+        }
+
+        System.out.println("Graph adjacency list representation: ");
+        System.out.println(createGraph(edges));
+    }
+}
