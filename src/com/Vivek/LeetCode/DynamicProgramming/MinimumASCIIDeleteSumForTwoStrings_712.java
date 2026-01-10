f99a6b99957d861ecce9e0edbed9eb54db4859fe
Vivek Kumar <vivekkr0311@gmail.com>
2026-01-10 19:42:09 +0530

    [Dynamic Programming]: [Memoization][Recursion] 712. Minimum ASCII Delete Sum for two strings

--- src/com/Vivek/LeetCode/DynamicProgramming/MinimumASCIIDeleteSumForTwoStrings_712.java
+++ src/com/Vivek/LeetCode/DynamicProgramming/MinimumASCIIDeleteSumForTwoStrings_712.java
@@ -1,8 +1,89 @@
 package com.Vivek.LeetCode.DynamicProgramming;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MinimumASCIIDeleteSumForTwoStrings_712 {
+    private static int recursionHelper(String s1, String s2, int i, int j) {
+        if (i < 0 && j < 0) {
+            return 0;
+        }
+
+        if (i < 0) {
+            int sum = 0;
+            while (j >= 0) {
+                sum += s2.charAt(j--);
+            }
+            return sum;
+        }
+
+        if (j < 0) {
+            int sum = 0;
+            while (i >= 0) {
+                sum += s1.charAt(i--);
+            }
+            return sum;
+        }
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return recursionHelper(s1, s2, i - 1, j - 1);
+        } else {
+            return Math.min(
+              s1.charAt(i) + recursionHelper(s1, s2,i - 1, j),
+              s2.charAt(j) + recursionHelper(s1, s2, i, j-1)
+            );
+        }
+    }
+
+    private static int minimumDeletionSumRecursion(String s1, String s2) {
+        return recursionHelper(s1, s2,s1.length() - 1, s2.length() - 1);
+    }
+
+    private static int[][] dp;
+    private static int dpSolutionFromRecursion(String s1, String s2, int i, int j) {
+        if( i < 0 && j < 0) {
+            return 0;
+        }
+
+        if (i < 0) {
+            int sum = 0;
+            while (j >= 0) {
+                sum += s2.charAt(j--);
+            }
+            return sum;
+        }
+
+        if(j < 0) {
+            int sum = 0;
+            while (i >= 0) {
+                sum += s1.charAt(i--);
+            }
+            return sum;
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if(s1.charAt(i) == s2.charAt(j)) {
+            dp[i][j] = dpSolutionFromRecursion(s1, s2, i - 1, j - 1);
+        } else {
+            dp[i][j] = Math.min(
+                    s1.charAt(i) + dpSolutionFromRecursion(s1, s2, i - 1, j),
+                    s2.charAt(j) + dpSolutionFromRecursion(s1, s2, i, j- 1)
+            );
+        }
+        return dp[i][j];
+    }
+
+    private static int dpMinimumDeleteSumFromRecursion(String s1, String s2) {
+        dp = new int[s1.length()][s2.length()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return dpSolutionFromRecursion(s1, s2, s1.length() - 1, s2.length() - 1);
+    }
 
     private static int minimumDeleteSum(String s1, String s2) {
         int m = s1.length();
@@ -42,5 +123,7 @@ public class MinimumASCIIDeleteSumForTwoStrings_712 {
         String str2 = scanner.nextLine();
 
         System.out.println("Minimum sum to delete: " + minimumDeleteSum(str1, str2));
+        System.out.println("Minimum sum to delete: " + minimumDeletionSumRecursion(str1, str2));
+        System.out.println("Minimum sum to delete: " + dpMinimumDeleteSumFromRecursion(str1, str2));
     }
 }
